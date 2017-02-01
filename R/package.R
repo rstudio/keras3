76ae03e90fa4e3fd45e73c4f58c7f5a7d6007c2a
JJ Allaire <jj@rstudio.org>
2017-02-01 12:29:00 -0500

    initial work

---
+++ R/package.R
@@ -0,0 +1,10 @@
+#' keras for R
+#' 
+#' \href{https://keras.io}{Keras}Keras is a high-level neural networks library
+#' built on top of TensorFlow. It was developed with a focus on enabling fast
+#' experimentation. 
+#' 
+#' @docType package
+#' @name keras
+#' @import tensorflow
+NULL
