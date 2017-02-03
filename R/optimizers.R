536292bcb7c2068d41141db59bb051a3fff4a6c3
JJ Allaire <jj@rstudio.org>
2017-02-03 10:14:27 -0500

    add some additional optimizers

--- R/optimizers.R
+++ R/optimizers.R
@@ -33,7 +33,7 @@ optimizer_sgd <- function(lr = 0.01, momentum = 0.0, decay = 0.0, nesterov = FAL
 #' 
 #' @inheritParams optimizer_sgd
 #' @param rho rho Decay factor.
-#' @param episilon Fuzz factor. 
+#' @param epsilon Fuzz factor. 
 #' 
 #' @note It is recommended to leave the parameters of this optimizer at their
 #' default values (except the learning rate, which can be freely tuned).
@@ -42,12 +42,141 @@ optimizer_sgd <- function(lr = 0.01, momentum = 0.0, decay = 0.0, nesterov = FAL
 #' 
 #' @export
 optimizer_rmsprop <- function(lr = 0.001, rho = 0.9, epsilon = 1e-08, decay = 0.0,
-                             clipnorm = NULL, clipvalue = NULL) {
+                              clipnorm = NULL, clipvalue = NULL) {
   keras$optimizers$RMSprop(
     lr = lr,
     rho = rho,
     epsilon = epsilon,
     decay = decay,
+    clipnorm = normalize_clip(clipnorm),
+    clipvalue = normalize_clip(clipvalue)
+  )
+}
+
+
+#' Adagrad optimizer.
+#' 
+#' Adagrad optimizer as described in
+#' \href{http://www.jmlr.org/papers/volume12/duchi11a/duchi11a.pdf}{Adaptive 
+#' Subgradient Methods for Online Learning and Stochastic Optimization}.
+#' 
+#' @inheritParams optimizer_rmsprop
+#' 
+#' @note It is recommended to leave the parameters of this optimizer at their 
+#'   default values.
+#' 
+#' @export
+optimizer_adagrad <- function(lr = 0.01, epsilon = 1e-08, decay = 0.0,
+                              clipnorm = NULL, clipvalue = NULL) {
+  keras$optimizers$Adagrad(
+    lr = lr,
+    epsilon = epsilon,
+    decay = decay,
+    clipnorm = normalize_clip(clipnorm),
+    clipvalue = normalize_clip(clipvalue)
+  )
+}
+
+#' Adadelta optimizer.
+#' 
+#' Adadelta optimizer as described in
+#' \href{https://arxiv.org/abs/1212.5701}{ADADELTA: An Adaptive Learning Rate
+#' Method}.
+#' 
+#' @inheritParams optimizer_rmsprop
+#'   
+#' @note It is recommended to leave the parameters of this optimizer at their 
+#'   default values.
+#'   
+#' @export
+optimizer_adadelta <- function(lr = 1.0, rho = 0.95, epsilon = 1e-08, decay = 0.0,
+                               clipnorm = NULL, clipvalue = NULL) {
+  keras$optimizers$Adadelta(
+    lr = lr,
+    rho = rho,
+    epsilon = epsilon,
+    decay = decay,
+    clipnorm = normalize_clip(clipnorm),
+    clipvalue = normalize_clip(clipvalue)
+  )
+}
+
+#' Adam optimizer
+#' 
+#' Adam optimizer as described in \href{https://arxiv.org/abs/1412.6980v8}{Adam
+#' - A Method for Stochastic Optimization}.
+#' 
+#' @inheritParams optimizer_rmsprop
+#' @param beta_1 The exponential decay rate for the 1st moment estimates.
+#' @param beta_2 The exponential decay rate for the 2nd moment estimates.
+#' 
+#' @note Default parameters follow those provided in the original paper.
+#' 
+#' @seealso \code{\link{optimizer_adamax}}
+#' 
+#' @export
+optimizer_adam <- function(lr = 0.001, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-08, decay = 0.0,
+                           clipnorm = NULL, clipvalue = NULL) {
+  keras$optimizers$Adam(
+    lr = lr,
+    beta_1 = beta_1,
+    beta_2 = beta_2,
+    epsilon = epsilon,
+    decay = decay,
+    clipnorm = normalize_clip(clipnorm),
+    clipvalue = normalize_clip(clipvalue)
+  )
+}
+
+#' Adamax optimizer
+#' 
+#' Adamax optimizer from Section 7 of the \href{https://arxiv.org/abs/1412.6980v8}{Adam paper}.
+#' It is a variant of Adam based on the infinity norm.
+#' 
+#' @inheritParams optimizer_adam
+#' 
+#' @seealso \code{\link{optimizer_adam}}
+#' 
+#' @export
+optimizer_adamax <- function(lr = 0.002, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-08, decay = 0.0,
+                             clipnorm = NULL, clipvalue = NULL) {
+  keras$optimizers$Adamax(
+    lr = lr,
+    beta_1 = beta_1,
+    beta_2 = beta_2,
+    epsilon = epsilon,
+    decay = decay,
+    clipnorm = normalize_clip(clipnorm),
+    clipvalue = normalize_clip(clipvalue)
+  )
+}
+
+#' Nesterov Adam optimizer
+#' 
+#' Much like Adam is essentially RMSprop with momentum, Nadam is Adam RMSprop 
+#' with Nesterov momentum. See 
+#' \href{http://cs229.stanford.edu/proj2015/054_report.pdf}{Incorporating 
+#' Nesterov Momentum into Adam}.
+#' 
+#' @inheritParams optimizer_adam
+#' @param schedule_decay Schedule deacy.
+#'   
+#' @details Default parameters follow those provided in the paper. It is 
+#'   recommended to leave the parameters of this optimizer at their default 
+#'   values.
+#'   
+#' @seealso \href{http://www.cs.toronto.edu/~fritz/absps/momentum.pdf}{On the
+#'   importance of initialization and momentum in deep learning}.
+#'   
+#' @export
+optimizer_nadam <- function(lr = 0.002, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-08, 
+                            schedule_decay = 0.004, clipnorm = NULL, clipvalue = NULL) {
+  keras$optimizers$Nadam(
+    lr = lr,
+    beta_1 = beta_1,
+    beta_2 = beta_2,
+    epsilon = epsilon,
+    schedule_decay = schedule_decay,
     clipnorm = normalize_clip(clipnorm),
     clipvalue = normalize_clip(clipvalue)
   )
