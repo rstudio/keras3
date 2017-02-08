0c5f84f7c55b87f9951e58fab6fd3e41f0a4cc41
JJ Allaire <jj@rstudio.org>
2017-02-08 13:53:46 -0500

    delay load keras

--- R/package.R
+++ R/package.R
@@ -12,31 +12,10 @@ NULL
 # Main Keras module
 keras <- NULL
 
-# load error message
-.load_error_message <- NULL
-
 .onLoad <- function(libname, pkgname) {
   
-  # attempt to load keras
-  keras  <<- tryCatch(import("keras"), error = function(e) e)
-  if (inherits(keras , "error")) {
-    .load_error_message <<- keras$message
-    keras  <<- NULL
-    return()
-  }
+  # delay load keras
+  keras <<- import("keras", delay_load = TRUE)
   
 }
-
-.onAttach <- function(libname, pkgname) {
-  
-  if (is.null(keras)) {
-    packageStartupMessage("\n", .load_error_message)
-    packageStartupMessage("\nIf you have not yet installed Keras, see https://keras.io\n")
-    packageStartupMessage("You should ensure that the version of python where ",
-                          "Keras is installed is either the default python ",
-                          "on the system PATH or is specified explicitly via the ",
-                          "RETICULATE_PYTHON environment variable.\n")
-  }
-}
-
 
