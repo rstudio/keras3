e4703b717e797455d93b97fb1632ca25f0aa704a
JJ Allaire <jj@rstudio.org>
2017-02-18 13:54:18 -0500

    more work on model functions

--- R/models.R
+++ R/models.R
@@ -13,7 +13,7 @@ model_sequential <- function(layers = NULL, name = NULL) {
 
 #' Configure a model for training
 #' 
-#' @param optimizer: Name of optimizer or optimizer object.
+#' @param optimizer Name of optimizer or optimizer object.
 #' @param loss Name of objective function or objective function. If the model 
 #'   has multiple outputs, you can use a different loss on each output by 
 #'   passing a dictionary or a list of objectives.
@@ -21,6 +21,7 @@ model_sequential <- function(layers = NULL, name = NULL) {
 #'   and testing. Typically you will use `metrics='accuracy'`. To specify 
 #'   different metrics for different outputs of a multi-output model, you could 
 #'   also pass a named list such as `metrics=list(output_a = 'accuracy')`.
+#' @param loss_weights Loss weights
 #' @param sample_weight_mode If you need to do timestep-wise sample weighting 
 #'   (2D weights), set this to "temporal". `NULL` defaults to sample-wise
 #'   weights (1D). If the model has multiple outputs, you can use a different 
@@ -41,14 +42,83 @@ compile <- function(model, optimizer, loss, metrics = NULL, loss_weights = NULL,
 }
 
 
+#' Train a model
+#' 
+#' Trains the model for a fixed number of epochs (iterations on a dataset).
+
+#' @param x Vector, matrix, or array of training data (or list if the model has 
+#'   multiple inputs). If all inputs in the model are named, you can also pass a
+#'   list mapping input names to data.
+#' @param y  Vector, matrix, or array of target data (or list if the model has 
+#'   multiple outputs). If all outputs in the model are named, you can also pass
+#'   a list mapping output names to data.
+#' @param batch_size Number of samples per gradient update.
+#' @param nb_epoch Number of times to iterate over the training data arrays.
+#' @param verbose  Verbosity mode (0 = silent, 1 = verbose, 2 = one log line per
+#'   epoch).
+#' @param callbacks List of callbacks to be called during training.
+#' @param validation_split Float between 0 and 1: fraction of the training data 
+#'   to be used as validation data. The model will set apart this fraction of 
+#'   the training data, will not train on it, and will evaluate the loss and any
+#'   model metrics on this data at the end of each epoch.
+#' @param validation_data Data on which to evaluate the loss and any model 
+#'   metrics at the end of each epoch. The model will not be trained on this 
+#'   data. This could be a list (x_val, y_val) or a list (x_val, y_val, 
+#'   val_sample_weights).
+#' @param shuffle `TRUE` to shuffle the training data before each epoch.
+#' @param class_weight Optional named list mapping indices (integers) to a
+#'   weight (float) to apply to the model's loss for the samples from this class
+#'   during training. This can be useful to tell the model to "pay more 
+#'   attention" to samples from an under-represented class.
+#' @param sample_weight Optional array of the same length as x, containing 
+#'   weights to apply to the model's loss for each sample. In the case of 
+#'   temporal data, you can pass a 2D array with shape (samples, 
+#'   sequence_length), to apply a different weight to every timestep of every 
+#'   sample. In this case you should make sure to specify 
+#'   sample_weight_mode="temporal" in [compile()].
+#' @param initial_epoch epoch at which to start training (useful for resuming a
+#'   previous training run).
+#' 
+#' @return Model object with "history" attribute that provides details on
+#'  the training.
+#' 
 #' @export
-fit <- function(model, x, y, batch_size = 32, nb_epoch = 10) {
-  model$fit(
+fit <- function(model, x, y, batch_size=32, nb_epoch=10, verbose=1, callbacks=NULL,
+                validation_split=0.0, validation_data=NULL, shuffle=TRUE,
+                class_weight=NULL, sample_weight=NULL, initial_epoch=0) {
+  
+  # force inputs to be numpy arrays
+  if (!is.array(x))
+    x <- np$array(x)
+  if (!is.array(y))
+    y <- np$array(y)
+  
+  # convert class weights to python dict
+  if (!is.null(class_weight)) {
+    if (is.list(class_weight))
+      class_weight <- dict(class_weight)
+    else
+      stop("class_weight must be a named list of weights")
+  }
+  
+  # fit the model
+  history <- model$fit(
     x = x,
     y = y,
+    batch_size = as.integer(batch_size),
     nb_epoch = as.integer(nb_epoch),
-    batch_size = as.integer(batch_size)
+    verbose = as.integer(verbose),
+    callbacks = callbacks,
+    validation_split = validation_split,
+    validation_data = validation_data,
+    shuffle = shuffle,
+    class_weight = class_weight,
+    sample_weight = sample_weight,
+    initial_epoch = as.integer(initial_epoch)
   )
+  
+  # return the model with history attached
+  attr(model, "history") <- history
   model
 }
 
@@ -100,6 +170,7 @@ load_model <- function(filepath, custom_objects = NULL) {
 #' Generates output predictions for the input samples, processing the samples in
 #' a batched way.
 #'
+#' @param object Keras model
 #' @param x Input data (vector, matrix, or array)
 #' @param batch_size Integer
 #' @param verbose Verbosity mode, 0 or 1.
