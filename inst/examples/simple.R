f8fe19e5f1cc2963d14033d0ba138008250b97ba
JJ Allaire <jj@rstudio.org>
2017-02-19 08:54:30 -0500

    eliminate comments on mutation

--- inst/examples/simple.R
+++ inst/examples/simple.R
@@ -9,8 +9,7 @@ labels <- matrix(round(runif(1000*10, min = 0, max = 9)), nrow = 1000, ncol = 10
 # genereate dummy input data
 input <- matrix(rexp(10*784), nrow = 10, ncol = 784)
 
-# define and compile the model (these methods copy the model prior to
-# returning it so do not mutate the model)
+# define and compile the model 
 model <- model_sequential() %>% 
   layer_dense(32, input_dim = 784) %>% 
   layer_activation('relu') %>% 
@@ -24,12 +23,10 @@ model <- model_sequential() %>%
 save_model(model, "model.hdf5")
 model <- load_model("model.hdf5")
 
-# train the model (this definitely mutates the model object). note we 
-# might be able to make this functional by using save_model / load_model
+# train the model 
 training_history <- fit(model, data, labels)
 
-# make some predictions (this could in theory mutate the model
-# object however it probably doesn't)
+# make some predictions
 predictions <- predict(model, input)
 
 
