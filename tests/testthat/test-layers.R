ad37a8495f65db2a0838122757af4f862d5f0b9f
JJ Allaire <jj@rstudio.org>
2017-03-27 12:41:00 -0400

    move test_call to utils

--- tests/testthat/test-layers.R
+++ tests/testthat/test-layers.R
@@ -2,42 +2,33 @@ context("layers")
 
 source("utils.R")
 
-
-test_call <- function(layer_name, expr) {
-  test_that(paste(layer_name, "call succeeds"), {
-    skip_if_no_keras()
-    force(expr)
-    expect_equal(TRUE, TRUE)
-  })
-}
-
-test_call("layer_input", {
+test_call_succeeds("layer_input", {
   layer_input(shape = shape(32))
 })
 
-test_call("layer_dense", {
+test_call_succeeds("layer_dense", {
   layer_dense(model_sequential(), 32, input_shape = shape(784))
 })
 
-test_call("layer_activation", {
+test_call_succeeds("layer_activation", {
   model_sequential() %>% 
     layer_dense(32, input_shape = shape(784)) %>% 
     layer_activation('relu')
 })
 
-test_call("layer_reshape", {
+test_call_succeeds("layer_reshape", {
   model_sequential() %>% 
     layer_dense(32, input_shape = shape(784)) %>% 
     layer_reshape(target_shape = shape(2,16))
 })
  
-test_call("layer_permute", {
+test_call_succeeds("layer_permute", {
   model_sequential() %>% 
     layer_dense(32, input_shape = shape(784)) %>% 
     layer_permute(dims = 1)
 })
 
-test_call("layer_flatten", {
+test_call_succeeds("layer_flatten", {
   model_sequential() %>% 
     layer_dense(32, input_shape = shape(784)) %>% 
     layer_reshape(target_shape = shape(2,16)) %>% 
--- tests/testthat/utils.R
+++ tests/testthat/utils.R
@@ -5,3 +5,11 @@ skip_if_no_keras <- function() {
   if (!reticulate::py_module_available("keras"))
     skip("keras not available for testing")
 }
+
+test_call_succeeds <- function(call_name, expr) {
+  test_that(paste(call_name, "call succeeds"), {
+    skip_if_no_keras()
+    force(expr)
+    expect_equal(TRUE, TRUE)
+  })
+}
