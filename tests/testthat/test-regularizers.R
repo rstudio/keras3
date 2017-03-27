0c1ddd2734eb76a37d1e8f760a10ddec52b8b2f6
JJ Allaire <jj@rstudio.org>
2017-03-27 13:58:06 -0400

    more dynamic regularizer tests

--- tests/testthat/test-regularizers.R
+++ tests/testthat/test-regularizers.R
@@ -2,19 +2,20 @@ context("regularizers")
 
 source("utils.R")
 
-test_regularizer <- function(name, regularizer) {
+test_regularizer <- function(name) {
+  regularizer_fn <- eval(parse(text = name))
   test_call_succeeds(name, {
     model_sequential() %>% 
       layer_dense(32, input_shape = shape(784), 
-                  kernel_regularizer = regularizer,
-                  activity_regularizer = regularizer)
+                  kernel_regularizer = regularizer_fn(),
+                  activity_regularizer = regularizer_fn())
   }) 
  
 }
 
-test_regularizer("regularizer_l1", regularizer_l1())
-test_regularizer("regularizer_l1_l2", regularizer_l1_l2())
-test_regularizer("regularizer_l2", regularizer_l2())
+test_regularizer("regularizer_l1")
+test_regularizer("regularizer_l1_l2")
+test_regularizer("regularizer_l2")
 
 
 
