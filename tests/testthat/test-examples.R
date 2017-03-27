2e8dbbdc0e12ca5e5c72ae8104982af9de3a1842
JJ Allaire <jj@rstudio.org>
2017-03-27 13:53:58 -0400

    test examples

---
+++ tests/testthat/test-examples.R
@@ -0,0 +1,27 @@
+context("examples")
+
+source("utils.R")
+
+# some helpers
+run_example <- function(example) {
+  env <- new.env()
+  capture.output({
+    example_path <- system.file("examples", example, package = "keras")
+    old_wd <- setwd(dirname(example_path))
+    on.exit(setwd(old_wd), add = TRUE)
+    source(basename(example_path), local = env)
+  }, type = "output")
+  rm(list = ls(env), envir = env)
+  gc()
+}
+
+examples <- if (nzchar(Sys.getenv("KERAS_TEST_EXAMPLES"))) {
+  c("simple.R")
+}
+
+for (example in examples) {
+  test_that(paste(example, "example runs successfully"), {
+    skip_if_no_keras()
+    expect_error(run_example(example), NA)
+  })
+}
