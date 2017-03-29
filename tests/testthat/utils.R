0351e5bec6e53bff4a87cd2edd2bed4423a9c6ea
jjallaire <jjallaire@jjallaires-MBP.home>
2017-03-29 08:51:27 -0400

    change keras probe to look in tensorflow.contrib.keras

--- tests/testthat/utils.R
+++ tests/testthat/utils.R
@@ -2,7 +2,7 @@
 use_virtualenv("~/tensorflow")
 
 skip_if_no_keras <- function() {
-  if (!reticulate::py_module_available("keras"))
+  if (!reticulate::py_module_available("tensorflow.contrib.keras"))
     skip("keras not available for testing")
 }
 
