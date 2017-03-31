8148e9d247b82b4b5b24909e38624d67accb3b1d
JJ Allaire <jj@rstudio.com>
2017-03-31 09:55:43 -0400

    add global poolings

--- R/pooling.R
+++ R/pooling.R
@@ -214,8 +214,139 @@ layer_average_pooling_3d <- function(x, pool_size = c(2L, 2L, 2L), strides = NUL
 }
 
 
+#' Global max pooling operation for temporal data.
+#' 
+#' @inheritParams layer_dense
+#' 
+#' @section Input shape:
+#' 3D tensor with shape: `(batch_size, steps, features)`.
+#' 
+#' @section Output shape:
+#' 2D tensor with shape: `(batch_size, channels)`
+#' 
+#' @export
+layer_global_max_pooling_1d <- function(x) {
+  call_layer(tf$contrib$keras$layers$GlobalMaxPooling1D, x, list())
+}
+
+#' Global average pooling operation for temporal data.
+#' 
+#' @inheritParams layer_global_max_pooling_1d
+#' 
+#' @section Input shape:
+#' 3D tensor with shape: `(batch_size, steps, features)`.
+#' 
+#' @section Output shape:
+#' 2D tensor with shape: `(batch_size, channels)`
+#' 
+#' @export
+layer_global_average_pooling_1d <- function(x) {
+  call_layer(tf$contrib$keras$layers$GlobalAveragePooling1D, x, list())
+}
 
 
+#' Global max pooling operation for spatial data.
+#' 
+#' @inheritParams layer_global_max_pooling_1d
+#' 
+#' @param data_format A string, one of `channels_last` (default) or
+#'   `channels_first`. The ordering of the dimensions in the inputs.
+#'   `channels_last` corresponds to inputs with shape `(batch, width, height,
+#'   channels)` while `channels_first` corresponds to inputs with shape `(batch,
+#'   channels, width, height)`. It defaults to the `image_data_format` value
+#'   found in your Keras config file at `~/.keras/keras.json`. If you never set
+#'   it, then it will be "channels_last".
+#'   
+#' @section Input shape: 
+#' - If `data_format='channels_last'`: 4D tensor with shape: `(batch_size, rows, cols, channels)` 
+#' - If `data_format='channels_first'`: 4D tensor with shape: `(batch_size, channels, rows, cols)`
+#'   
+#' @section Output shape: 2D tensor with shape: `(batch_size, channels)`
+#'   
+#' @export
+layer_global_max_pooling_2d <- function(x, data_format = NULL) {
+  call_layer(tf$contrib$keras$layers$GlobalMaxPooling2D, x, list(
+    data_format = data_format
+  ))
+}
+
+#' Global average pooling operation for spatial data.
+#' 
+#' @inheritParams layer_global_average_pooling_1d
+#' 
+#' @param data_format A string, one of `channels_last` (default) or
+#'   `channels_first`. The ordering of the dimensions in the inputs.
+#'   `channels_last` corresponds to inputs with shape `(batch, width, height,
+#'   channels)` while `channels_first` corresponds to inputs with shape `(batch,
+#'   channels, width, height)`. It defaults to the `image_data_format` value
+#'   found in your Keras config file at `~/.keras/keras.json`. If you never set
+#'   it, then it will be "channels_last".
+#'   
+#' @section Input shape: 
+#' - If `data_format='channels_last'`: 4D tensor with shape: `(batch_size, rows, cols, channels)` 
+#' - If `data_format='channels_first'`: 4D tensor with shape: `(batch_size, channels, rows, cols)`
+#'   
+#' @section Output shape: 2D tensor with shape: `(batch_size, channels)`
+#'   
+#' @export
+layer_global_average_pooling_2d <- function(x, data_format = NULL) {
+  call_layer(tf$contrib$keras$layers$GlobalAveragePooling2D, x, list(
+    data_format = data_format
+  ))
+}
+
+
+#' Global Max pooling operation for 3D data.
+#' 
+#' @inheritParams layer_global_max_pooling_2d
+#'   
+#' @param data_format A string, one of `channels_last` (default) or
+#'   `channels_first`. The ordering of the dimensions in the inputs.
+#'   `channels_last` corresponds to inputs with shape `(batch, spatial_dim1,
+#'   spatial_dim2, spatial_dim3, channels)` while `channels_first` corresponds
+#'   to inputs with shape `(batch, channels, spatial_dim1, spatial_dim2,
+#'   spatial_dim3)`. It defaults to the `image_data_format` value found in your
+#'   Keras config file at `~/.keras/keras.json`. If you never set it, then it
+#'   will be "channels_last".
+#'   
+#' @section Input shape: 
+#' - If `data_format='channels_last'`: 5D tensor with shape: `(batch_size, spatial_dim1, spatial_dim2, spatial_dim3, channels)` 
+#' - If `data_format='channels_first'`: 5D tensor with shape: `(batch_size, channels, spatial_dim1, spatial_dim2, spatial_dim3)`
+#'   
+#' @section Output shape: 2D tensor with shape: `(batch_size, channels)`
+#'   
+#' @export
+layer_global_max_pooling_3d <- function(x, data_format = NULL) {
+  call_layer(tf$contrib$keras$layers$GlobalMaxPooling3D, x, list(
+    data_format = data_format
+  ))
+}
+
+#' Global Average pooling operation for 3D data.
+#' 
+#' @inheritParams layer_global_average_pooling_2d
+#'   
+#' @param data_format A string, one of `channels_last` (default) or
+#'   `channels_first`. The ordering of the dimensions in the inputs.
+#'   `channels_last` corresponds to inputs with shape `(batch, spatial_dim1,
+#'   spatial_dim2, spatial_dim3, channels)` while `channels_first` corresponds
+#'   to inputs with shape `(batch, channels, spatial_dim1, spatial_dim2,
+#'   spatial_dim3)`. It defaults to the `image_data_format` value found in your
+#'   Keras config file at `~/.keras/keras.json`. If you never set it, then it
+#'   will be "channels_last".
+#'   
+#' @section Input shape: 
+#' - If `data_format='channels_last'`: 5D tensor with shape: `(batch_size, spatial_dim1, spatial_dim2, spatial_dim3, channels)` 
+#' - If `data_format='channels_first'`: 5D tensor with shape: `(batch_size, channels, spatial_dim1, spatial_dim2, spatial_dim3)`
+#'   
+#' @section Output shape: 2D tensor with shape: `(batch_size, channels)`
+#'   
+#' @export
+layer_global_average_pooling_3d <- function(x, data_format = NULL) {
+  call_layer(tf$contrib$keras$layers$GlobalAveragePooling3D, x, list(
+    data_format = data_format
+  ))
+}
 
 
 
