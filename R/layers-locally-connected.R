dca955b4cb2f5b419fc9a7ae5af3a048549c9517
JJ Allaire <jj@rstudio.com>
2017-04-01 15:04:54 -0400

    family for locally connected layers

--- R/layers-locally-connected.R
+++ R/layers-locally-connected.R
@@ -16,23 +16,13 @@
 #'   with specifying any `dilation_rate` value != 1.
 #' @param padding One of `"valid"` or `"same"` (case-insensitive).
 #' @param data_format data_format
-#' @param activation Activation function to use. If you don't specify anything,
-#'   no activation is applied (ie. "linear" activation: `a(x) = x`).
-#' @param use_bias Boolean, whether the layer uses a bias vector.
-#' @param kernel_initializer Initializer for the `kernel` weights matrix.
-#' @param bias_initializer Initializer for the bias vector.
-#' @param kernel_regularizer Regularizer function applied to the `kernel`
-#'   weights matrix.
-#' @param bias_regularizer Regularizer function applied to the bias vector.
-#' @param activity_regularizer Regularizer function applied to the output of the
-#'   layer (its "activation")..
-#' @param kernel_constraint Constraint function applied to the kernel matrix.
-#' @param bias_constraint Constraint function applied to the bias vector.
 #'   
 #' @section Input shape: 3D tensor with shape: `(batch_size, steps, input_dim)`
 #'   
 #' @section Output shape: 3D tensor with shape: `(batch_size, new_steps,
 #'   filters)` `steps` value might have changed due to padding or strides.
+#'   
+#' @family locally connected layers 
 #'   
 #' @export
 layer_locally_connected_1d <- function(x, filters, kernel_size, strides = 1L, padding = "valid", data_format = NULL, 
@@ -65,7 +55,7 @@ layer_locally_connected_1d <- function(x, filters, kernel_size, strides = 1L, pa
 #' that weights are unshared, that is, a different set of filters is applied at
 #' each different patch of the input.
 #' 
-#' @inheritParams layer_conv_2d
+#' @inheritParams layer_locally_connected_1d
 #' 
 #' @param filters Integer, the dimensionality of the output space (i.e. the
 #'   number output of filters in the convolution).
@@ -76,7 +66,6 @@ layer_locally_connected_1d <- function(x, filters, kernel_size, strides = 1L, pa
 #'   the convolution along the width and height. Can be a single integer to
 #'   specify the same value for all spatial dimensions. Specifying any stride
 #'   value != 1 is incompatible with specifying any `dilation_rate` value != 1.
-#' @param padding one of `"valid"` or `"same"` (case-insensitive).
 #' @param data_format A string, one of `channels_last` (default) or
 #'   `channels_first`. The ordering of the dimensions in the inputs.
 #'   `channels_last` corresponds to inputs with shape `(batch, width, height,
@@ -84,18 +73,6 @@ layer_locally_connected_1d <- function(x, filters, kernel_size, strides = 1L, pa
 #'   channels, width, height)`. It defaults to the `image_data_format` value
 #'   found in your Keras config file at `~/.keras/keras.json`. If you never set
 #'   it, then it will be "channels_last".
-#' @param activation Activation function to use. If you don't specify anything,
-#'   no activation is applied (ie. "linear" activation: `a(x) = x`).
-#' @param use_bias Boolean, whether the layer uses a bias vector.
-#' @param kernel_initializer Initializer for the `kernel` weights matrix.
-#' @param bias_initializer Initializer for the bias vector.
-#' @param kernel_regularizer Regularizer function applied to the `kernel`
-#'   weights matrix.
-#' @param bias_regularizer Regularizer function applied to the bias vector.
-#' @param activity_regularizer Regularizer function applied to the output of the
-#'   layer (its "activation")..
-#' @param kernel_constraint Constraint function applied to the kernel matrix.
-#' @param bias_constraint Constraint function applied to the bias vector.
 #'   
 #' @section Input shape: 4D tensor with shape: `(samples, channels, rows, cols)`
 #'   if data_format='channels_first' or 4D tensor with shape: `(samples, rows,
@@ -105,7 +82,9 @@ layer_locally_connected_1d <- function(x, filters, kernel_size, strides = 1L, pa
 #'   new_cols)` if data_format='channels_first' or 4D tensor with shape:
 #'   `(samples, new_rows, new_cols, filters)` if data_format='channels_last'.
 #'   `rows` and `cols` values might have changed due to padding.
-#'   
+#' 
+#' @family locally connected layers 
+#'       
 #' @export
 layer_locally_connected_2d <- function(x, filters, kernel_size, strides = c(1L, 1L), padding = "valid", data_format = NULL, 
                                        activation = NULL, use_bias = TRUE, kernel_initializer = "glorot_uniform", 
