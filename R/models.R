4b5467c16974019cdefb9bb96b2b6cf61bf2ce14
JJ Allaire <jj@rstudio.com>
2017-04-04 10:15:37 -0400

    return model invisibly

--- R/models.R
+++ R/models.R
@@ -74,7 +74,9 @@ compile <- function(model, optimizer, loss, metrics = NULL, loss_weights = NULL,
     loss_weights = loss_weights,
     sample_weight_mode = sample_weight_mode
   )
-  model
+  
+  # return it invisibly
+  invisible(model)
 }
 
 
