0510567e1842fa7d21874b30868811043240a661
JJ Allaire <jj@rstudio.com>
2017-04-05 15:32:28 -0400

    add to_categorical util

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,27 @@
 
 
+
+#' Converts a class vector (integers) to binary class matrix.
+#' 
+#' @details 
+#' E.g. for use with [loss_categorical_crossentropy()].
+#' 
+#' @param y Class vector to be converted into a matrix (integers from 0 to num_classes).
+#' @param num_classes Total number of classes.
+#' 
+#' @return A binary matrix representation of the input.
+#' 
+#' @export
+to_categorical <- function(y, num_classes = NULL) {
+  keras$utils$to_categorical(
+    y = y,
+    num_classes = as_nullable_integer(num_classes)
+  )
+}
+
+ 
+
+
 is_keras_function <- function(f) {
   is.function(f) && identical(environment(f), getNamespace("keras"))
 }
