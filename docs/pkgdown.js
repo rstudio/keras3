55bc47b0a3db1bd9e764a19f85426456f848a7d0
JJ Allaire <jj@rstudio.com>
2017-04-12 12:32:08 -0400

    about keras models/layers articles

--- docs/pkgdown.js
+++ docs/pkgdown.js
@@ -10,6 +10,9 @@ $(function() {
 $(document).ready(function() {
   
   
+  // turn functions section into ref-table
+  $('#functions').find('table').attr('class', 'ref-index');
+  
   // remove full s3 class from methods
   $(".ref-index a:contains('tensorflow.contrib.keras')").remove();
 });
--- tools/docs/pkgdown.js
+++ tools/docs/pkgdown.js
@@ -2,6 +2,9 @@
 $(document).ready(function() {
   
   
+  // turn functions section into ref-table
+  $('#functions').find('table').attr('class', 'ref-index');
+  
   // remove full s3 class from methods
   $(".ref-index a:contains('tensorflow.contrib.keras')").remove();
 });
