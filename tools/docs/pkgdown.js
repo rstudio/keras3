ec0d51f1c56a0592f9718ed69c79478c473902b4
JJ Allaire <jj@rstudio.com>
2017-04-12 15:27:26 -0400

    some tweaks to examples css

--- tools/docs/pkgdown.js
+++ tools/docs/pkgdown.js
@@ -1,11 +1,21 @@
 
 $(document).ready(function() {
-  
-  
+
   // turn functions section into ref-table
   $('#functions').find('table').attr('class', 'ref-index');
   
   // remove full s3 class from methods
   $(".ref-index a:contains('tensorflow.contrib.keras')").remove();
+  
+  // are we in examples?
+  var examples = window.location.href.match("/articles/examples/") !== null;
+  if (examples) {
+    $('.template-vignette').addClass('examples');
+    var header = $(".page-header > h1");
+    var script = header.text();
+    var a = $("<a></a>");
+    a.text(script);
+    a.attr('href', "https://github.com/rstudio/keras/blob/master/vignettes/examples/" + script);
+    header.empty().append(a);
+  }
 });
-
