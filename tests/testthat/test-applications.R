626aaad3539aaae152f9ed6438169f84b329502a
JJ Allaire <jj@rstudio.com>
2017-04-27 15:57:59 -0400

    don't test applications by default

--- tests/testthat/test-applications.R
+++ tests/testthat/test-applications.R
@@ -5,6 +5,11 @@ source("utils.R")
 
 
 test_succeeds("keras pre-built models can be instantiated", {
+  
+  skip <- is.na(Sys.getenv("KERAS_TEST_APPLICATIONS", unset = NA))
+  if (skip)
+    return()
+  
   application_xception()
   application_resnet50()
   application_vgg16()
