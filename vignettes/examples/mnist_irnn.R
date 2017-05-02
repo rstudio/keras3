acbdbc9a34d272d40246b810888c7dc5459f0b9f
JJ Allaire <jj@rstudio.com>
2017-05-02 16:12:46 -0400

    begin work on mnist_irnn example

--- vignettes/examples/mnist_irnn.R
+++ vignettes/examples/mnist_irnn.R
@@ -1,1 +1,15 @@
+#' This is a reproduction of the IRNN experiment
+#' with pixel-by-pixel sequential MNIST in
+#' "A Simple Way to Initialize Recurrent Networks of Rectified Linear Units"
+#' by Quoc V. Le, Navdeep Jaitly, Geoffrey E. Hinton
+#' 
+#' arxiv:1504.00941v2 [cs.NE] 7 Apr 2015
+#' http://arxiv.org/pdf/1504.00941v2.pdf
+# 
+#' Optimizer is replaced with RMSprop which yields more stable and steady
+#' improvement.
+# 
+#' Reaches 0.93 train/test accuracy after 900 epochs
+#' (which roughly corresponds to 1687500 steps in the original paper.)
+
 library(keras)
