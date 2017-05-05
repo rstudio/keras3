94a4cf627cbfb12d405987d828809f800d11d043
JJ Allaire <jj@rstudio.com>
2017-05-05 15:49:45 -0400

    remainder of stateful_lstm example

--- vignettes/examples/stateful_lstm.R
+++ vignettes/examples/stateful_lstm.R
@@ -36,9 +36,41 @@ cat('Input shape:', dim(cos), '\n')
 
 expected_output <- array(data = numeric(length(cos)), dim = c(length(cos), 1))
 for (i in 1:(length(cos) - lahead)) {
-  expected_output[[i, 1]] <- mean(cos[i + 1,i + lahead + 1])  # ??? R equivalent
+  expected_output[[i, 1]] <- mean(cos[(i + 1):(i + lahead)])
 }
 
 cat('Output shape:', dim(expected_output), '\n')
+
+cat('Creating model:\n')
+model <- keras_model_sequential()
+model %>%
+  layer_lstm(units = 50, input_shape = c(tsteps, 1), batch_size = batch_size,
+             return_sequences = TRUE, stateful = TRUE) %>% 
+  layer_lstm(units = 50, return_sequences = FALSE, stateful = TRUE) %>% 
+  layer_dense(units = 1)
+model %>% compile(loss = 'mse', optimizer = 'rmsprop')
+
+cat('Training\n')
+for (i in 1:epochs) {
+  model %>% fit(cos, expected_output, batch_size = batch_size,
+                epochs = 1, verbose = 1, shuffle = FALSE)
+            
+  model %>% reset_states()
+}
+
+cat('Predicting\n')
+predicted_output <- model %>% predict(cos, batch_size = batch_size)
+
+cat('Plotting Results\n')
+op <- par(mfrow=c(2,1))
+plot(expected_output, xlab = '')
+title("Expected")
+plot(predicted_output, xlab = '')
+title("Predicted")
+par(op)
+
+
+
+
 
 
