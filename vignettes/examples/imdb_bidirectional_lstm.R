14542d3d8d132559f4ae8fffdcc351581e9295a4
JJ Allaire <jj@rstudio.com>
2017-05-05 12:40:40 -0400

    imdb_bidirectional_lstm example

--- vignettes/examples/imdb_bidirectional_lstm.R
+++ vignettes/examples/imdb_bidirectional_lstm.R
@@ -1,1 +1,9 @@
+#' Train a Bidirectional LSTM on the IMDB sentiment classification task.
+#' 
+#' Output after 4 epochs on CPU: ~0.8146
+#' Time per epoch on CPU (Core i7): ~150s.
+
 library(keras)
+
+
+
