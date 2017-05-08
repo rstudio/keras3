32c1f4421138f8c28cf89a9d8cb870ada2e55848
JJ Allaire <jj@rstudio.com>
2017-05-08 10:37:55 -0400

    check tensorflow version on startup, import rather than depend on tensorflow package

--- R/package.R
+++ R/package.R
@@ -11,9 +11,16 @@ keras <- NULL
 .onLoad <- function(libname, pkgname) {
   
   # delay load keras
-  keras <<- import("tensorflow.contrib.keras.python.keras", 
-                   as = "tensorflow.keras", 
-                   delay_load = TRUE)
+  keras <<- import("tensorflow.contrib.keras.python.keras", as = "tensorflow.keras", delay_load = function() {
   
+    # confirm required tf version
+    tf_version <- tensorflow_version()
+    required_tf_version <- "1.2"
+    if (tf_version < required_tf_version) {
+      message("Keras loaded from TensorFlow version ", tf_version, ", however version ",
+              required_tf_version, " is required. Please update TensorFlow.")
+    }
+                       
+  })
 }
 
