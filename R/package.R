4258b665fba84fd4a55f924882481399b83489c6
JJ Allaire <jj@rstudio.com>
2017-05-09 11:05:22 -0400

    provide on_error handler for delay_load

--- R/package.R
+++ R/package.R
@@ -11,16 +11,22 @@ keras <- NULL
 .onLoad <- function(libname, pkgname) {
   
   # delay load keras
-  keras <<- import("tensorflow.contrib.keras.python.keras", as = "tensorflow.keras", delay_load = function() {
-  
-    # confirm required tf version
-    tf_version <- tensorflow_version()
-    required_tf_version <- "1.1"
-    if (tf_version < required_tf_version) {
-      message("Keras loaded from TensorFlow version ", tf_version, ", however version ",
-              required_tf_version, " is required. Please update TensorFlow.")
+  keras <<- import("tensorflow.contrib.keras.python.keras", as = "tensorflow.keras", delay_load = list(
+   
+    on_load = function() {
+      # confirm required tf version
+      tf_version <- tensorflow_version()
+      required_tf_version <- "1.1"
+      if (tf_version < required_tf_version) {
+        message("Keras loaded from TensorFlow version ", tf_version, ", however version ",
+                required_tf_version, " is required. Please update TensorFlow.")
+      }
+    },
+    
+    on_error = function(e) {
+      stop(tensorflow_config_error_message(), call. = FALSE)
     }
-                       
-  })
+     
+  ))
 }
 
