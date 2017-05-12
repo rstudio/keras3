f37c1e8491db53dd74ed6c773f930841dde10414
JJ Allaire <jj@rstudio.com>
2017-05-12 15:38:23 -0400

    use tf_config function

--- R/package.R
+++ R/package.R
@@ -14,8 +14,9 @@ keras <- NULL
   keras <<- import("tensorflow.contrib.keras.python.keras", as = "tensorflow.keras", delay_load = list(
    
     on_load = function() {
+      
       # confirm required tf version
-      tf_ver <- tf_version()
+      tf_ver <- tf_config()$version
       required_tf_ver <- "1.1"
       if (tf_ver < required_tf_ver) {
         message("Keras loaded from TensorFlow version ", tf_ver, ", however version ",
@@ -24,7 +25,7 @@ keras <- NULL
     },
     
     on_error = function(e) {
-      stop(tf_config_error_message(), call. = FALSE)
+      stop(tf_config()$error_message, call. = FALSE)
     }
      
   ))
