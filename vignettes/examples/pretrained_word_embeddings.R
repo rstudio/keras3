2c122007d4ea90b91af3512591ef8c3acaf8a9ec
JJ Allaire <jj@rstudio.com>
2017-05-18 15:12:32 -0400

    more work on data ingestion

--- vignettes/examples/pretrained_word_embeddings.R
+++ vignettes/examples/pretrained_word_embeddings.R
@@ -34,7 +34,64 @@ download_data <- function(data_dir, url_path, data_file) {
 download_data(GLOVE_DIR, 'http://nlp.stanford.edu/data/', 'glove.6B.zip')
 download_data(TEXT_DATA_DIR, "http://www.cs.cmu.edu/afs/cs.cmu.edu/project/theo-20/www/data/", "news20.tar.gz")
 
+# first, build index mapping words in the embeddings set
+# to their embedding vector
+
+cat('Indexing word vectors.\n')
+
+embeddings_index <- new.env(parent = emptyenv())
+lines <- readLines(file.path(GLOVE_DIR, 'glove.6B.100d.txt'))
+for (line in lines) {
+  values <- strsplit(line, ' ', fixed = TRUE)[[1]]
+  word <- values[[1]]
+  coefs <- as.numeric(values[-1])
+  embeddings_index[[word]] <- coefs
+}
+
+cat(sprintf('Found %s word vectors.\n', length(embeddings_index)))
+
+# second, prepare text samples and their labels
+cat('Processing text dataset\n')
+
+texts <- character()  # list of text samples
+labels_index <- list()  # dictionary: label name to numeric id
+labels <- character() # list of label ids
+
+for (name in list.files(TEXT_DATA_DIR)) {
+  path <- file.path(TEXT_DATA_DIR, name)
+  if (file_test("-d", path)) {
+    label_id <- length(labels_index)
+    labels_index[[name]] <- label_id
+    for (fname in list.files(path)) {
+      if (grepl("^[0-9]+$", fname)) {
+        fpath <- file.path(path, fname)
+        t <- readLines(fpath, encoding = "latin1")
+        t <- paste(t, collapse = "\n")
+        i <- regexpr(pattern = '\n\n', t, fixed = TRUE)[[1]]
+        if (i != -1L)
+          t <- substring(t, i)
+        texts <- c(texts, t)
+        labels <- c(labels, label_id)
+      }
+    }
+  }
+}
+
+cat(sprintf('Found %s texts.', length(texts)))
+
+# finally, vectorize the text samples into a 2D integer tensor
+tokenizer <- text_tokenizer(num_words=MAX_NB_WORDS)
+tokenizer %>% fit(texts)
+sequences <- texts_to_sequences(tokenizer, texts)
+
+word_index <- tokenizer$word_index
+cat(sprintf('Found %s unique tokens.', length(word_index)))
 
 
+# data <- pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+# 
+# labels = to_categorical(np.asarray(labels))
+# print('Shape of data tensor:', data.shape)
+# print('Shape of label tensor:', labels.shape)
 
 
