cf8bb1b622f55bad45ad02b18c7f663296747348
JJ Allaire <jj@rstudio.com>
2017-05-18 11:22:50 -0400

    download data if required

--- vignettes/examples/pretrained_word_embeddings.R
+++ vignettes/examples/pretrained_word_embeddings.R
@@ -12,3 +12,29 @@
 #' 
 
 library(keras)
+
+GLOVE_DIR <- 'glove.6B'
+TEXT_DATA_DIR <- '20_newsgroup'
+MAX_SEQUENCE_LENGTH <- 1000
+MAX_NB_WORDS <- 20000
+EMBEDDING_DIM <- 100
+VALIDATION_SPLIT <- 0.2
+
+# download data if necessary
+download_data <- function(data_dir, url_path, data_file) {
+  if (!dir.exists(data_dir)) {
+    download.file(paste0(url_path, data_file), data_file, mode = "wb")
+    if (tools::file_ext(data_file) == "zip")
+      unzip(data_file, exdir = tools::file_path_sans_ext(data_file))
+    else
+      untar(data_file)
+    unlink(data_file)
+  }
+}
+download_data(GLOVE_DIR, 'http://nlp.stanford.edu/data/', 'glove.6B.zip')
+download_data(TEXT_DATA_DIR, "http://www.cs.cmu.edu/afs/cs.cmu.edu/project/theo-20/www/data/", "news20.tar.gz")
+
+
+
+
+
