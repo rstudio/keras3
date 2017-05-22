c6c7c28271b0c89961b10137d0fbb9be8f6a4b18
Daniel Falbel <dfalbel@gmail.com>
2017-05-22 14:05:40 +0000

    setup lstm_text_generation

--- vignettes/examples/lstm_text_generation.R
+++ vignettes/examples/lstm_text_generation.R
@@ -1,1 +1,11 @@
+# Example script to generate text from Nietzsche's writings.
+# 
+# At least 20 epochs are required before the generated text
+# starts sounding coherent.
+# 
+# It is recommended to run this script on GPU, as recurrent
+# networks are quite computationally intensive.
+# 
+# If you try this script on new data, make sure your corpus
+# has at least ~100k characters. ~1M is better.
 library(keras)
