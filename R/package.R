d601a6a6f6d2a1a88ead747e304e6e944c506f8f
jjallaire <jj@rstudio.com>
2017-06-19 16:26:01 -0400

    correct tf version checking in onload

--- R/package.R
+++ R/package.R
@@ -9,24 +9,13 @@ NULL
 # Main Keras module
 keras <- NULL
 
-# startup messages
-.startupMessage <- NULL
-
 .onLoad <- function(libname, pkgname) {
   
   # delay load keras
   keras <<- import("tensorflow.contrib.keras.python.keras", as = "tensorflow.keras", delay_load = list(
    
     on_load = function() {
-      
-      # confirm required tf version
-      tf_ver <- tf_config()$version
-      required_tf_ver <- "1.1"
-      if (tf_ver < required_tf_ver) {
-        .startupMessage <- paste0(
-          "Keras loaded from TensorFlow version ", tf_ver, ", however version ",
-          required_tf_ver, " is required. Please update TensorFlow.")
-      }
+      check_tf_version()
     },
     
     on_error = function(e) {
@@ -36,8 +25,11 @@ keras <- NULL
   ))
 }
 
-.onAttach <- function(libname, pkgname) {
-  if (!is.null(.startupMessage))
-    packageStartupMessage(.startupMessage)
+check_tf_version <- function() {
+  tf_ver <- tf_config()$version
+  required_tf_ver <- "1.1"
+  if (tf_ver < required_tf_ver) {
+    message("Keras loaded from TensorFlow version ", tf_ver, ", however version ",
+            required_tf_ver, " is required. Please update TensorFlow.")
+  }
 }
-
