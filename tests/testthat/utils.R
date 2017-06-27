53ebb47a9fa96f889babd7d2ccbf93bd648e9dd8
jjallaire <jj@rstudio.com>
2017-06-27 11:47:35 -0400

    remove use_virtualenv from test utils

--- tests/testthat/utils.R
+++ tests/testthat/utils.R
@@ -1,6 +1,4 @@
 
-use_virtualenv("~/tensorflow")
-
 skip_if_no_keras <- function() {
   if (!reticulate::py_module_available("tensorflow.contrib.keras"))
     skip("keras not available for testing")
