0fee22eeed5a93268519dcd1b67424a376922ec1
jjallaire <jj@rstudio.com>
2017-07-24 07:28:07 -0400

    integerate neural style transfer example

--- vignettes/examples/neural_style_transfer.R
+++ vignettes/examples/neural_style_transfer.R
@@ -1,53 +1,33 @@
-# Neural style transfer with Keras.
-# 
-# Run the script with:
-# ```
-# python neural_style_transfer.py path_to_your_base_image.jpg path_to_your_reference.jpg prefix_for_results
-# ```
-# e.g.:
-# ```
-# python neural_style_transfer.py img/tuebingen.jpg img/starry_night.jpg results/my_result
-# ```
-# Optional parameters:
-# ```
-# --iter, To specify the number of iterations the style transfer takes place (Default is 10)
-# --content_weight, The weight given to the content loss (Default is 0.025)
-# --style_weight, The weight given to the style loss (Default is 1.0)
-# --tv_weight, The weight given to the total variation loss (Default is 1.0)
-# ```
-# 
-# It is preferable to run this script on GPU, for speed.
-# 
-# Example result: https://twitter.com/fchollet/status/686631033085677568
-# 
-# # Details
-# 
-# Style transfer consists in generating an image
-# with the same "content" as a base image, but with the
-# "style" of a different picture (typically artistic).
-# 
-# This is achieved through the optimization of a loss function
-# that has 3 components: "style loss", "content loss",
-# and "total variation loss":
-# 
-# - The total variation loss imposes local spatial continuity between
-# the pixels of the combination image, giving it visual coherence.
-# 
-# - The style loss is where the deep learning keeps in --that one is defined
-# using a deep convolutional neural network. Precisely, it consists in a sum of
-# L2 distances between the Gram matrices of the representations of
-# the base image and the style reference image, extracted from
-# different layers of a convnet (trained on ImageNet). The general idea
-# is to capture color/texture information at different spatial
-# scales (fairly large scales --defined by the depth of the layer considered).
-# 
-# - The content loss is a L2 distance between the features of the base
-# image (extracted from a deep layer) and the features of the combination image,
-# keeping the generated image close enough to the original one.
-# 
-# # References
-# - [A Neural Algorithm of Artistic Style](http://arxiv.org/abs/1508.06576)
-# 
+#' Neural style transfer with Keras.
+#' 
+#' It is preferable to run this script on a GPU, for speed.
+#' 
+#' Example result: https://twitter.com/fchollet/status/686631033085677568
+#' 
+#' Style transfer consists in generating an image
+#' with the same "content" as a base image, but with the
+#' "style" of a different picture (typically artistic). 
+#' 
+#' This is achieved through the optimization of a loss function
+#' that has 3 components: "style loss", "content loss",
+#' and "total variation loss":
+#' 
+#'  - The total variation loss imposes local spatial continuity between
+#'    the pixels of the combination image, giving it visual coherence.
+#' 
+#'  - The style loss is where the deep learning keeps in --that one is defined
+#'    using a deep convolutional neural network. Precisely, it consists in a sum of
+#'    L2 distances between the Gram matrices of the representations of
+#'    the base image and the style reference image, extracted from
+#'    different layers of a convnet (trained on ImageNet). The general idea
+#'    is to capture color/texture information at different spatial
+#'    scales (fairly large scales --defined by the depth of the layer considered).
+#' 
+#'  - The content loss is a L2 distance between the features of the base
+#'    image (extracted from a deep layer) and the features of the combination image,
+#'    keeping the generated image close enough to the original one.
+#'
+ 
 library(keras)
 library(purrr)
 library(R6)
