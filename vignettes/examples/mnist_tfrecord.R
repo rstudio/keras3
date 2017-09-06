a2cbf63f02502ec60bb0222a298b01c8c92057e6
jjallaire <jj@rstudio.com>
2017-09-06 15:20:01 -0400

    bump reticulate version

--- vignettes/examples/mnist_tfrecord.R
+++ vignettes/examples/mnist_tfrecord.R
@@ -140,7 +140,7 @@ K$clear_session()
 x_test <- data$validation$images
 x_test <- keras_array(x_test, dim = c(nrow(x_test), 28, 28, 1))
 y_test <- data$validation$labels
-x_test_inp <- layer_input(shape = c(28, 28, 1))
+x_test_inp <- layer_input(shape = dim(x_test)[-1])
 test_out <- cnn_layers(x_test_inp)
 test_model <- keras_model(inputs = x_test_inp, outputs = test_out)
 test_model %>% load_model_weights_hdf5('saved_wt.h5')
