97f6624882e5d090d0855458dba380d5ce199a25
Christopher Sparling <christopher.sparling@climaxmedia.com>
2017-09-28 09:43:34 -0400

    Minor formatting, elaboration

--- vignettes/examples/mnist_cnn.R
+++ vignettes/examples/mnist_cnn.R
@@ -2,30 +2,35 @@
 #' Trains a simple convnet on the MNIST dataset.
 #' 
 #' Gets to 99.25% test accuracy after 12 epochs
-#' (there is still a lot of margin for parameter tuning).
+#'  Note: There is still a large margin for parameter tuning
+#' 
 #' 16 seconds per epoch on a GRID K520 GPU.
 
 library(keras)
+
+# Data Preparation -----------------------------------------------------
 
 batch_size <- 128
 num_classes <- 10
 epochs <- 12
 
-# input image dimensions
+# Input image dimensions
 img_rows <- 28
 img_cols <- 28
 
-# the data, shuffled and split between train and test sets
+# The data, shuffled and split between train and test sets
 mnist <- dataset_mnist()
 x_train <- mnist$train$x
 y_train <- mnist$train$y
 x_test <- mnist$test$x
 y_test <- mnist$test$y
 
+# Redefine  dimension of train/test inputs
 dim(x_train) <- c(nrow(x_train), img_rows, img_cols, 1) 
 dim(x_test) <- c(nrow(x_test), img_rows, img_cols, 1)
 input_shape <- c(img_rows, img_cols, 1)
 
+# Transform RGB values into [0,1] range
 x_train <- x_train / 255
 x_test <- x_test / 255
 
@@ -33,11 +38,12 @@ cat('x_train_shape:', dim(x_train), '\n')
 cat(nrow(x_train), 'train samples\n')
 cat(nrow(x_test), 'test samples\n')
 
-# convert class vectors to binary class matrices
+# Convert class vectors to binary class matrices
 y_train <- to_categorical(y_train, num_classes)
 y_test <- to_categorical(y_test, num_classes)
 
-# define model
+# Define Model -----------------------------------------------------------
+
 model <- keras_model_sequential()
 model %>%
   layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = 'relu',
@@ -50,14 +56,15 @@ model %>%
   layer_dropout(rate = 0.5) %>% 
   layer_dense(units = num_classes, activation = 'softmax')
 
-# compile model
+# Compile model
 model %>% compile(
   loss = loss_categorical_crossentropy,
   optimizer = optimizer_adadelta(),
   metrics = c('accuracy')
 )
 
-# train and evaluate
+# Train & Evaluate -------------------------------------------------------
+
 model %>% fit(
   x_train, y_train,
   batch_size = batch_size,
@@ -69,7 +76,6 @@ scores <- model %>% evaluate(
   x_test, y_test, verbose = 0
 )
 
+# Output metrics
 cat('Test loss:', scores[[1]], '\n')
 cat('Test accuracy:', scores[[2]], '\n')
-
-
