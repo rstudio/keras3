fa1dccd27b32ba7c1ef2b75056c4ef557985e6eb
jjallaire <jj@rstudio.com>
2017-12-05 15:14:41 -0800

    add swiftype search

--- website/extra.js
+++ website/extra.js
@@ -13,4 +13,16 @@ $(document).ready(function() {
     $(".col-md-9").removeClass("col-md-9").addClass('col-md-10');
     $(".col-md-3").remove();
   }
+  
+   // add search box
+  var search_form = $.parseHTML('<input type="text" class="st-default-search-input" placeholder="Search">');
+  var navbar_right = $('.navbar-right');
+  navbar_right.append(search_form);
+  
+  // add swiftype
+  (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
+  (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
+  e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
+  })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+  _st('install','RzoPBEQeaoi3AAmD3UEK','2.0.0');
 });
