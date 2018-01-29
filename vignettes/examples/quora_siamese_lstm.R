a29102c2ab98dfca2e2948d2d55863b6220ebf0b
Daniel Falbel <dfalbel@gmail.com>
2018-01-29 21:55:32 -0200

    adding prediction section

--- vignettes/examples/quora_siamese_lstm.R
+++ vignettes/examples/quora_siamese_lstm.R
@@ -104,3 +104,32 @@ model %>%
 
 save_model_hdf5(model, "model-question-pairs.hdf5", include_optimizer = TRUE)
 save_text_tokenizer(tokenizer, "tokenizer-question-pairs.hdf5")
+
+
+# Prediction --------------------------------------------------------------
+# In a fresh R session:
+# Load model and tokenizer -
+
+model <- load_model_hdf5("model-question-pairs.hdf5", compile = FALSE)
+tokenizer <- load_text_tokenizer("tokenizer-question-pairs.hdf5")
+
+
+predict_question_pairs <- function(model, tokenizer, q1, q2) {
+  
+  q1 <- texts_to_sequences(tokenizer, list(q1))
+  q2 <- texts_to_sequences(tokenizer, list(q2))
+  
+  q1 <- pad_sequences(q1, 20)
+  q2 <- pad_sequences(q2, 20)
+  
+  as.numeric(predict(model, list(q1, q2)))
+}
+
+# Getting predictions
+
+predict_question_pairs(
+  model, tokenizer, 
+  q1 = "What is the main benefit of Quora?",
+  q2 = "What are the advantages of using Quora?"
+)
+
