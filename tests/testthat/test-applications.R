f6a74b60b4a6091b6d064179f9789534173f1538
Javier Luraschi <javierluraschi@hotmail.com>
2018-02-12 19:09:48 -0800

    make densenet and nasnet run only under 2.1.3 tests

--- tests/testthat/test-applications.R
+++ tests/testthat/test-applications.R
@@ -15,14 +15,21 @@ test_succeeds("keras pre-built models can be instantiated", {
     application_xception()
     if (is_keras_available("2.0.6"))
       application_mobilenet()
+    
+    if (is_keras_available("2.1.3")) {
+      application_densenet121()
+      application_densenet169()
+      application_densenet201()
+      
+      application_nasnetlarge()
+      application_nasnetmobile()
+    }
   }
+  
   application_resnet50()
   application_vgg16()
   application_vgg19()
   application_inception_v3()
-  
-  application_densenet()
-  application_nasnet()
 })
 
 
