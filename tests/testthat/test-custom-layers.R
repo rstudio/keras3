e8a4384a139147428938824e2eb497bdb975eee2
jjallaire <jj@rstudio.com>
2018-04-04 08:28:43 -0400

    skip custom layer test for tf implementation

--- tests/testthat/test-custom-layers.R
+++ tests/testthat/test-custom-layers.R
@@ -46,8 +46,9 @@ layer_custom <- function(object, output_dim, name = NULL, trainable = TRUE) {
   ))
 }
 
-test_succeeds("Use an R-based custom Keras layer", {
 
+test_succeeds("Use an R-based custom Keras layer", {
+  skip_if_tensorflow_implementation()
   model <- keras_model_sequential()
   model %>% 
     layer_dense(units = 32, input_shape = c(32,32)) %>% 
