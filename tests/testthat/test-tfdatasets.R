1b602758b2ff91411b5ac44d7576bff9a8c6b3d2
Daniel Falbel <dfalbel@gmail.com>
2019-05-10 10:27:55 -0300

    add test to ensure it errors when specifying the batch_size with a dataset

--- tests/testthat/test-tfdatasets.R
+++ tests/testthat/test-tfdatasets.R
@@ -1,4 +1,5 @@
 context("tfdatasets")
+source("utils.R")
 
 test_succeeds("Use tfdatasets to train a keras model", {
   
@@ -20,5 +21,25 @@ test_succeeds("Use tfdatasets to train a keras model", {
     evaluate(model, dataset, steps = 10)
     preds <- predict(model, dataset, steps = 10)
   }
+  
+})
 
+test_that("Error when specifying batch_size with tfdatasets", {
+  skip_if_no_keras()
+  if (!keras::is_tensorflow_implementation())
+    skip("Datasets need TensorFlow implementation.")
+  
+  model <- keras_model_sequential() %>% 
+    layer_dense(units = 1, input_shape = 1)
+  model %>% compile(loss='mse', optimizer='sgd')
+  
+  dataset <- tfdatasets::tensors_dataset(reticulate::tuple(list(1), list(1))) %>% 
+    tfdatasets::dataset_repeat(100) %>% 
+    tfdatasets::dataset_shuffle(buffer_size = 100) %>% 
+    tfdatasets::dataset_batch(10)
+  
+  expect_error(
+    model %>% fit(dataset, epochs = 2, batch_size = 5)
+  )
+  
 })
