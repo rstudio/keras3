b77bc5a486f55ce1b1491f847054578c23729865
Daniel Falbel <dfalbel@gmail.com>
2019-12-20 14:43:46 -0300

    docs + allow inheriting from custom classes

--- R/Layer.R
+++ R/Layer.R
@@ -1,7 +1,23 @@
-#' Create a custom Layer
+#' #' Create a custom Layer
 #' 
 #' @param classname the name of the custom Layer.
-#' @param defs definition of methods.
+#' @param initialize a function. This is where you define the arguments used to further
+#'  build yolur layer. For example a dense layer, would take the `units` argument.
+#'  You should always call \code{super()$`__init__()`} to intialized the base 
+#'  inherited layer.
+#' @param build a function that takes `input_shape` as argument. This is where you will 
+#'  define your weights. Note that if your layer doesn’t define trainable weights then
+#'  you need not implemented this method.
+#' @param call This is where the layer’s logic lives. Unless you want your layer to 
+#'  support masking, you only have to care about the first argument passed to `call` 
+#'  the input tensor.
+#' @param compute_output_shape a function that takes `input_shape` as an argument. In 
+#'  case your layer modifies the shape of its input, you should specify here the 
+#'  shape transformation logic. This allows Keras to do automatic shape inference. 
+#'  If you don’t modify the shape of the input then you need not implement this 
+#'  method.
+#' @param ... Any other methods and/or attributes can be specified using named
+#'  arguments. They will be added to the layer class.
 #' @param inherit the Keras layer to inherit from
 #' 
 #' @examples
@@ -9,45 +25,49 @@
 #' 
 #' layer_dense2 <- Layer(
 #'   "Dense2",
-#'   list(
-#'     
-#'     initialize = function(units) {
-#'       self$units <- units
-#'     },
-#'     
-#'     build = function(input_shape) {
-#'       self$kernel <- self$add_weight(
-#'         name = "kernel",
-#'         shape = list(input_shape[[2]], self$units),
-#'         initializer = "uniform",
-#'         trainable = TRUE
-#'       )
-#'     },
-#'     
-#'     call = function(x) {
-#'       tensorflow::tf$matmul(x, self$kernel)
-#'     },
-#'     
-#'     compute_output_shape = function(input_shape) {
-#'       list(input_shape[[1]], self$units)
-#'     }
-#'     
-#'   )
+#'   
+#'   initialize = function(units) {
+#'     super()$`__init__`()
+#'     self$units <- as.integer(units)
+#'   },
+#'   
+#'   build = function(input_shape) {
+#'     print(class(input_shape))
+#'     self$kernel <- self$add_weight(
+#'       name = "kernel",
+#'       shape = list(input_shape[[2]], self$units),
+#'       initializer = "uniform",
+#'       trainable = TRUE
+#'     )
+#'   },
+#'   
+#'   call = function(x) {
+#'     tensorflow::tf$matmul(x, self$kernel)
+#'   },
+#'   
+#'   compute_output_shape = function(input_shape) {
+#'     list(input_shape[[1]], self$units)
+#'   }
+#'   
 #' )
 #' 
+#' l <- layer_dense2(units = 10)
+#' l(matrix(runif(10), ncol = 1))
 #' 
 #' }
 #' 
 #' 
 #' @export
-Layer <- function(classname, initialize, build = NULL, call = NULL, ...,  
+Layer <- function(classname, initialize, build = NULL, call = NULL, 
+                  compute_output_shape = NULL, ...,  
                   inherit = tensorflow::tf$keras$layers$Layer) {
   
   
   defs <- list(
     initialize = initialize,
     build = build,
-    call = call
+    call = call,
+    compute_output_shape = compute_output_shape
   )
   defs <- Filter(Negate(is.null), defs)
   defs <- append(defs, list(...))
@@ -76,6 +96,10 @@ Layer <- function(classname, initialize, build = NULL, call = NULL, ...,
   
   # makes the function return NULL. `__init__` in python must always return None
   defs$`__init__` <- wrap_return_null(defs$`__init__`)
+  
+  # allow inheriting from custom created layers
+  if (!is.null(attr(inherit, "layer")))
+    inherit <- attr(inherit, "layer")
   
   layer <- reticulate::PyClass(
     classname = classname,
