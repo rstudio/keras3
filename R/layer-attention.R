7a8ce059d6c15c4b55f6ed40f490e06b4b389cce
Andy Troiano <andytroiano@gmail.com>
2020-03-05 19:35:20 -0500

    added layer attention documentation

--- R/layer-attention.R
+++ R/layer-attention.R
@@ -4,6 +4,7 @@
 #' Dot-product attention layer, a.k.a. Luong-style attention.
 #' 
 #' @inheritParams layer_dense
+#' 
 #' @param inputs a list of inputs first should be the query tensor, the second the value tensor
 #' @param use_scale If True, will create a scalar variable to scale the attention scores.
 #' @param causal Boolean. Set to True for decoder self-attention. Adds a mask such that position i cannot attend to positions j > i. 
