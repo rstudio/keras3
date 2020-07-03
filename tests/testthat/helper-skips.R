7c08d36107b239a9190c51cbb823a3ae16bfd53e
Daniel Falbel <dfalbel@gmail.com>
2020-07-03 15:53:37 -0300

    fix skips

--- tests/testthat/helper-skips.R
+++ tests/testthat/helper-skips.R
@@ -1,4 +1,4 @@
 skip_if_not_tensorflow_version <- function(version) {
-  if (tensorflow::tf_config() < version)
+  if (tensorflow::tf_version() < version)
     skip(paste0("Needs TensorFlow version >= ", version))
 }
