30d8eb48885df99d93e2f992e216e33c6faa513c
Daniel Falbel <dfalbel@gmail.com>
2020-07-20 17:25:21 -0300

    skip test in older tf versions

--- tests/testthat/test-layer-text_vectorization.R
+++ tests/testthat/test-layer-text_vectorization.R
@@ -84,6 +84,9 @@ test_call_succeeds("can use layer_text_vectorization", {
 
 test_call_succeeds("can create a tf-idf layer", {
   
+  if (tensorflow::tf_version() < "2.1")
+    skip("TextVectorization requires TF version >= 2.1")
+  
   text_vectorization <- layer_text_vectorization( 
     max_tokens = num_words, output_mode = "tf-idf" 
   )
