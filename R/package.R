de58c7793cf0eafabbcec8e552a0e731a1a42d60
Tomasz Kalinowski <kalinowskit@gmail.com>
2023-05-11 14:20:35 -0400

    prefer `r-tensorflow` env in .onLoad() (again)

--- R/package.R
+++ R/package.R
@@ -109,7 +109,7 @@ keras <- NULL
 
     priority = 10,
 
-    environment = "r-reticulate",
+    environment = "r-tensorflow",
 
     get_module = function() {
       resolve_implementation_module()
