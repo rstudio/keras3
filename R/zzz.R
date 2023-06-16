b5a8e48cfbc941a0e4565f4bed730a8b9368e619
Tomasz Kalinowski <kalinowskit@gmail.com>
2023-06-16 12:07:11 -0400

    update S3 methods for new internal keras symbols

---
+++ R/zzz.R
@@ -0,0 +1,37 @@
+
+## keras internal symbols moved under `src` in TF 2.12
+#' @export
+compile.keras.src.engine.training.Model <- compile.keras.engine.training.Model
+
+#' @export
+evaluate.keras.src.engine.training.Model <- evaluate.keras.engine.training.Model
+
+#' @export
+export_savedmodel.keras.src.engine.training.Model <- export_savedmodel.keras.engine.training.Model
+
+#' @export
+fit.keras.src.engine.training.Model <- fit.keras.engine.training.Model
+
+#' @export
+format.keras.src.engine.training.Model <- format.keras.engine.training.Model
+
+#' @export
+plot.keras.src.engine.training.Model <- plot.keras.engine.training.Model
+
+#' @export
+predict.keras.src.engine.training.Model <- predict.keras.engine.training.Model
+
+#' @export
+print.keras.src.engine.training.Model <- print.keras.engine.training.Model
+
+#' @export
+py_str.keras.src.engine.training.Model <- py_str.keras.engine.training.Model
+
+#' @export
+py_to_r_wrapper.keras.src.engine.base_layer.Layer <- py_to_r_wrapper.keras.engine.base_layer.Layer
+
+#' @export
+py_to_r_wrapper.keras.src.engine.training.Model <- py_to_r_wrapper.keras.engine.training.Model
+
+#' @export
+summary.keras.src.engine.training.Model <- summary.keras.engine.training.Model
