5b3fb5417cceb3df011fe0a722e10dac5c3bbbbb
Tomasz Kalinowski <kalinowskit@gmail.com>
2023-08-09 13:44:53 -0400

    redocument.

--- R/model.R
+++ R/model.R
@@ -610,21 +610,21 @@ resolve_main_thread_generators <- function(x, callback_type = "on_train_batch_be
 #'   multiple inputs). If all inputs in the model are named, you can also pass a
 #'   list mapping input names to data. `x` can be `NULL` (default) if feeding
 #'   from framework-native tensors (e.g. TensorFlow data tensors). You can also
-#'   pass a `tfdataset` or a generator returning a list with `(inputs, targets)` or
-#'   `(inputs, targets, sample_weights)`.
-#' @param y  Vector, matrix, or array of target (label) data (or list if the model has
-#'   multiple outputs). If all outputs in the model are named, you can also pass
-#'   a list mapping output names to data. `y` can be `NULL` (default) if feeding
-#'   from framework-native tensors (e.g. TensorFlow data tensors).
+#'   pass a `tfdataset` or a generator returning a list with `(inputs, targets)`
+#'   or `(inputs, targets, sample_weights)`.
+#' @param y  Vector, matrix, or array of target (label) data (or list if the
+#'   model has multiple outputs). If all outputs in the model are named, you can
+#'   also pass a list mapping output names to data. `y` can be `NULL` (default)
+#'   if feeding from framework-native tensors (e.g. TensorFlow data tensors).
 #' @param batch_size Integer or `NULL`. Number of samples per gradient update.
 #'   If unspecified, `batch_size` will default to 32.
-#' @param epochs Number of epochs to train the model.
-#'   Note that in conjunction with `initial_epoch`,
-#'   `epochs` is to be understood as "final epoch". The model is
-#'   not trained for a number of iterations given by `epochs`, but
+#' @param epochs Number of epochs to train the model. Note that in conjunction
+#'   with `initial_epoch`, `epochs` is to be understood as "final epoch". The
+#'   model is not trained for a number of iterations given by `epochs`, but
 #'   merely until the epoch of index `epochs` is reached.
-#' @param verbose  Verbosity mode (0 = silent, 1 = progress bar, 2 = one line per
-#'   epoch). Defaults to
+#' @param verbose  Verbosity mode (0 = silent, 1 = progress bar, 2 = one line
+#'   per epoch). Defaults to 1 in most contexts, 2 if in knitr render or running
+#'   on a distributed training server.
 #' @param view_metrics View realtime plot of training metrics (by epoch). The
 #'   default (`"auto"`) will display the plot when running within RStudio,
 #'   `metrics` were specified during model [compile()], `epochs > 1` and
@@ -634,21 +634,21 @@ resolve_main_thread_generators <- function(x, callback_type = "on_train_batch_be
 #' @param validation_split Float between 0 and 1. Fraction of the training data
 #'   to be used as validation data. The model will set apart this fraction of
 #'   the training data, will not train on it, and will evaluate the loss and any
-#'   model metrics on this data at the end of each epoch. The validation data
-#'   is selected from the last samples in the `x` and `y` data provided,
-#'   before shuffling.
+#'   model metrics on this data at the end of each epoch. The validation data is
+#'   selected from the last samples in the `x` and `y` data provided, before
+#'   shuffling.
 #' @param validation_data Data on which to evaluate the loss and any model
 #'   metrics at the end of each epoch. The model will not be trained on this
 #'   data. This could be a list (x_val, y_val) or a list (x_val, y_val,
 #'   val_sample_weights). `validation_data` will override `validation_split`.
-#' @param shuffle shuffle: Logical (whether to shuffle the training data
-#'    before each epoch) or string (for "batch"). "batch" is a special option
-#'    for dealing with the limitations of HDF5 data; it shuffles in batch-sized
-#'    chunks. Has no effect when `steps_per_epoch` is not `NULL`.
+#' @param shuffle shuffle: Logical (whether to shuffle the training data before
+#'   each epoch) or string (for "batch"). "batch" is a special option for
+#'   dealing with the limitations of HDF5 data; it shuffles in batch-sized
+#'   chunks. Has no effect when `steps_per_epoch` is not `NULL`.
 #' @param class_weight Optional named list mapping indices (integers) to a
-#'   weight (float) value, used for weighting the loss function
-#'   (during training only). This can be useful to tell the model to
-#'   "pay more attention" to samples from an under-represented class.
+#'   weight (float) value, used for weighting the loss function (during training
+#'   only). This can be useful to tell the model to "pay more attention" to
+#'   samples from an under-represented class.
 #' @param sample_weight Optional array of the same length as x, containing
 #'   weights to apply to the model's loss for each sample. In the case of
 #'   temporal data, you can pass a 2D array with shape (samples,
@@ -660,14 +660,14 @@ resolve_main_thread_generators <- function(x, callback_type = "on_train_batch_be
 #' @param steps_per_epoch Total number of steps (batches of samples) before
 #'   declaring one epoch finished and starting the next epoch. When training
 #'   with input tensors such as TensorFlow data tensors, the default `NULL` is
-#'   equal to the number of samples in your dataset divided by the batch
-#'   size, or 1 if that cannot be determined.
+#'   equal to the number of samples in your dataset divided by the batch size,
+#'   or 1 if that cannot be determined.
 #' @param  validation_steps Only relevant if `steps_per_epoch` is specified.
 #'   Total number of steps (batches of samples) to validate before stopping.
 #' @param ... Unused
 #'
-#' @return A `history` object that contains all information collected
-#'   during training.
+#' @return A `history` object that contains all information collected during
+#'   training.
 #'
 #' @family model functions
 #'
