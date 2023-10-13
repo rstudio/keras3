975076b4b5055c25240d085e71435cde52fbab63
Tomasz Kalinowski <tomasz@posit.co>
2023-10-13 17:02:36 -0400

    fix keras tensor Ops tests

--- R/backend.R
+++ R/backend.R
@@ -121,4 +121,15 @@ k_variable <- function(...) keras$Variable(...)
 
 k_backend <- function() keras$config$backend()
 
+#' @export
+`==.keras.backend.common.keras_tensor.KerasTensor` <- function(e1, e2) {
+  k_equal(e1, e2)
+}
+
+#' @export
+`+.keras.backend.common.keras_tensor.KerasTensor` <- function(e1, e2) {
+  if(missing(e2)) return(e1)
+  NextMethod()
+}
+
 
