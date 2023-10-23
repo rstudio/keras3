e23fac6ff5c97f7a0d1d4aecdd48e717d9d2bb22
Tomasz Kalinowski <tomasz@posit.co>
2023-10-23 10:16:48 -0400

    new ops wrappers, quantile / median

--- R/autogen-ops.R
+++ R/autogen-ops.R
@@ -7505,6 +7505,50 @@ function (x, axis = NULL, keepdims = FALSE)
 }
 
 
+# keras$layers$median
+# keras_core.src.ops.numpy.median
+r"-(Compute the median along the specified axis.
+
+    Args:
+        x: Input tensor.
+        axis: Axis or axes along which the medians are computed. Defaults to
+            `axis=None` which is to compute the median(s) along a flattened
+            version of the array.
+        keepdims: If this is set to `True`, the axes which are reduce
+            are left in the result as dimensions with size one.
+
+    Returns:
+        The output tensor.
+    )-"
+
+
+# keras_core.src.ops.numpy.median
+#' Compute the median along the specified axis.
+#'
+#' @description
+#'
+#' # Returns
+#'     The output tensor.
+#'
+#' @param x Input tensor.
+#' @param axis Axis or axes along which the medians are computed. Defaults to
+#'     `axis=None` which is to compute the median(s) along a flattened
+#'     version of the array.
+#' @param keepdims If this is set to `True`, the axes which are reduce
+#'     are left in the result as dimensions with size one.
+#'
+#' @export
+#' @family ops
+#' @seealso
+#' + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/median>
+k_median <-
+function (x, axis = NULL, keepdims = FALSE)
+{
+    args <- capture_args2(list(axis = as_axis))
+    do.call(keras$ops$median, args)
+}
+
+
 # keras$layers$meshgrid
 # keras_core.src.ops.numpy.meshgrid
 r"-(Creates grids of coordinates from coordinate vectors.
@@ -8183,6 +8227,80 @@ function (x, axis = NULL, keepdims = FALSE, dtype = NULL)
 {
     args <- capture_args2(list(axis = as_axis))
     do.call(keras$ops$prod, args)
+}
+
+
+# keras$layers$quantile
+# keras_core.src.ops.numpy.quantile
+r"-(Compute the q-th quantile(s) of the data along the specified axis.
+
+    Args:
+        x: Input tensor.
+        q: Probability or sequence of probabilities for the quantiles to
+            compute. Values must be between 0 and 1 inclusive.
+        axis: Axis or axes along which the quantiles are computed. Defaults to
+            `axis=None` which is to compute the quantile(s) along a flattened
+            version of the array.
+        method: A string specifies the method to use for estimating the
+            quantile. Available methods are `"linear"`, `"lower"`, `"higher"`,
+            `"midpoint"`, and `"nearest"`. Defaults to `"linear"`.
+            If the desired quantile lies between two data points `i < j`:
+            - `"linear"`: `i + (j - i) * fraction`, where fraction is the
+                fractional part of the index surrounded by `i` and `j`.
+            - `"lower"`: `i`.
+            - `"higher"`: `j`.
+            - `"midpoint"`: `(i + j) / 2`
+            - `"nearest"`: `i` or `j`, whichever is nearest.
+        keepdims: If this is set to `True`, the axes which are reduce
+            are left in the result as dimensions with size one.
+
+    Returns:
+        The quantile(s). If `q` is a single probability and `axis=None`, then
+        the result is a scalar. If multiple probabilies levels are given, first
+        axis of the result corresponds to the quantiles. The other axes are the
+        axes that remain after the reduction of `x`.
+    )-"
+
+
+# keras_core.src.ops.numpy.quantile
+#' Compute the q-th quantile(s) of the data along the specified axis.
+#'
+#' @description
+#'
+#' # Returns
+#' The quantile(s). If `q` is a single probability and `axis=None`, then
+#' the result is a scalar. If multiple probabilies levels are given, first
+#' axis of the result corresponds to the quantiles. The other axes are the
+#' axes that remain after the reduction of `x`.
+#'
+#' @param x Input tensor.
+#' @param q Probability or sequence of probabilities for the quantiles to
+#'     compute. Values must be between 0 and 1 inclusive.
+#' @param axis Axis or axes along which the quantiles are computed. Defaults to
+#'     `axis=None` which is to compute the quantile(s) along a flattened
+#'     version of the array.
+#' @param method A string specifies the method to use for estimating the
+#'     quantile. Available methods are `"linear"`, `"lower"`, `"higher"`,
+#'     `"midpoint"`, and `"nearest"`. Defaults to `"linear"`.
+#'     If the desired quantile lies between two data points `i < j`:
+#'     - `"linear"`: `i + (j - i) * fraction`, where fraction is the
+#'         fractional part of the index surrounded by `i` and `j`.
+#'     - `"lower"`: `i`.
+#'     - `"higher"`: `j`.
+#'     - `"midpoint"`: `(i + j) / 2`
+#'     - `"nearest"`: `i` or `j`, whichever is nearest.
+#' @param keepdims If this is set to `True`, the axes which are reduce
+#'     are left in the result as dimensions with size one.
+#'
+#' @export
+#' @family ops
+#' @seealso
+#' + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/quantile>
+k_quantile <-
+function (x, q, axis = NULL, method = "linear", keepdims = FALSE)
+{
+    args <- capture_args2(list(axis = as_axis))
+    do.call(keras$ops$quantile, args)
 }
 
 
