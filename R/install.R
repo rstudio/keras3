dd7c431f8e8321faf0b15ba39965ffd207ecf755
Tomasz Kalinowski <tomasz@posit.co>
2023-11-02 15:48:39 -0400

    install more pypi packages

--- R/install.R
+++ R/install.R
@@ -51,6 +51,7 @@ install_keras <- function(...,
   # system2("python", c("-m pip install -r requirements.txt")) # unpin tf-nightly for Python 3.12
   system2("python", c("-m pip install -r requirements-common.txt"))
   system2("python", c("-m pip install ipython")) # for interactive debugging
+  system2("python", c("-m pip install openai tiktoken")) # for roxygen generation
   system2("python", c("-m pip install tf-nightly jax[cpu]")) # unpin tf-nightly for Python 3.12
   system2("python", c("-m pip uninstall -y keras keras-nightly"))
   system2("python", c("-m pip install torch torchvision")) # needed for pip_build.py?? (but why?)
