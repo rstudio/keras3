63150c07364a63b77879505bda9912c9221a81de
Tomasz Kalinowski <tomasz@posit.co>
2024-01-03 10:02:13 -0500

    warn about unlisted guides

--- tools/knit-vignettes.R
+++ tools/knit-vignettes.R
@@ -1,15 +1,36 @@
 #!/usr/bin/env Rscript
 
+envir::attach_source("tools/knit.R")
+
 library(tfdatasets, exclude = c("shape"))
 library(tensorflow, exclude = c("shape", "set_random_seed"))
 library(keras3)
 
+files <- list.files("vignettes-src",
+                    pattern = "^[^_.].+\\.[RrQq]md",
+                    full.names = TRUE)
 
-envir::attach_source("tools/knit.R")
-
-for (f in list.files("vignettes-src",
-                     pattern = "^[^_.].+\\.[RrQq]md",
-                     full.names = TRUE)) {
+for (f in files) {
   knit_vignette(f)
 }
 
+
+find_hrefs <- function(x) {
+  if(!is.list(x)) return()
+  unlist(c(x$href, lapply(x, find_hrefs)), use.names = FALSE)
+}
+
+
+listed <- yaml::read_yaml("pkgdown/_pkgdown.yml") |>
+  _$navbar |>
+  find_hrefs() |>
+  grep("index.html", x = _, invert = TRUE, fixed = TRUE, value = TRUE) |>
+  sub("articles/", "", x = _)
+
+unlisted <- setdiff(fs::path_ext_remove(basename(files)),
+                    fs::path_ext_remove(listed))
+unlisted <- sprintf("vignettes-src/%s.Rmd", unlisted)
+
+cli::cli_warn("Guides rendered but not listed in {.file pkgdown/_pkgdown.yml}")
+cli::cli_ul(sprintf("{.file %s}", unlisted))
+cli::cli_end()
