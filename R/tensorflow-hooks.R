dbdca6a464b50ccf5353486b5e7558a9ff67290f
Tomasz Kalinowski <kalinowskit@gmail.com>
2024-02-01 11:59:51 -0500

    update tensorflow hooks for tfruns

--- R/tensorflow-hooks.R
+++ R/tensorflow-hooks.R
@@ -1,8 +1,17 @@
 
 
 tensorflow_on_before_use_session <- function(quiet) {
-  if (is_backend("tensorflow")) {
-    keras$backend$clear_session()
+  if (identical(config_backend(), "tensorflow")) {
+    tryCatch(
+      keras$utils$clear_session(),
+      python.builtin.AttributeError = function(e) {
+        tryCatch(
+          keras$backend$clear_session(),
+          error = function(e2)
+            stop(e)
+        )
+      }
+    )
     TRUE
   } else {
     FALSE
