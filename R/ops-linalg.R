421c7d59f8907bc37cc62de7a267b2d619ecb38b
Tomasz Kalinowski <kalinowskit@gmail.com>
2024-02-19 13:41:12 -0500

    adapt new linear algebra ops

--- R/ops-linalg.R
+++ R/ops-linalg.R
@@ -1,3 +1,4 @@
+
 #' Computes the Cholesky decomposition of a positive semi-definite matrix.
 #'
 #' @returns
@@ -8,61 +9,73 @@
 #' Input tensor of shape `(..., M, M)`.
 #'
 #' @export
+#' @family linear algebra ops
+#' @family ops
 #' @tether keras.ops.cholesky
-#' @seealso
-#' + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/cholesky>
+# @seealso
+# + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/cholesky>
 op_cholesky <-
 function (x)
 keras$ops$cholesky(x)
 
+
 #' Computes the determinant of a square tensor.
 #'
 #' @returns
-#'     A tensor of shape `(...,)` represeting the determinant of `x`.
+#' A tensor of shape `(...)` represeting the determinant of `x`.
 #'
 #' @param x
 #' Input tensor of shape `(..., M, M)`.
 #'
 #' @export
+#' @family linear algebra ops
+#' @family ops
 #' @tether keras.ops.det
-#' @seealso
-#' + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/det>
+# @seealso
+# + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/det>
 op_det <-
 function (x)
 keras$ops$det(x)
 
+
 #' Computes the eigenvalues and eigenvectors of a square matrix.
 #'
 #' @returns
-#' A tuple of two tensors: a tensor of shape `(..., M)` containing
+#' A list of two tensors: a tensor of shape `(..., M)` containing
 #' eigenvalues and a tensor of shape `(..., M, M)` containing eigenvectors.
 #'
 #' @param x
 #' Input tensor of shape `(..., M, M)`.
 #'
 #' @export
+#' @family linear algebra ops
+#' @family ops
 #' @tether keras.ops.eig
-#' @seealso
-#' + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/eig>
+# @seealso
+# + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/eig>
 op_eig <-
 function (x)
 keras$ops$eig(x)
 
+
 #' Computes the inverse of a square tensor.
 #'
 #' @returns
-#'     A tensor of shape `(..., M, M)` representing the inverse of `x`.
+#' A tensor of shape `(..., M, M)` representing the inverse of `x`.
 #'
 #' @param x
 #' Input tensor of shape `(..., M, M)`.
 #'
 #' @export
+#' @family linear algebra ops
+#' @family ops
 #' @tether keras.ops.inv
-#' @seealso
-#' + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/inv>
+# @seealso
+# + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/inv>
 op_inv <-
 function (x)
 keras$ops$inv(x)
+
 
 #' Computes the lower-upper decomposition of a square matrix.
 #'
@@ -75,12 +88,15 @@ keras$ops$inv(x)
 #' A tensor of shape `(..., M, M)`.
 #'
 #' @export
+#' @family linear algebra ops
+#' @family ops
 #' @tether keras.ops.lu_factor
-#' @seealso
-#' + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/lu_factor>
+# @seealso
+# + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/lu_factor>
 op_lu_factor <-
 function (x)
 keras$ops$lu_factor(x)
+
 
 #' Matrix or vector norm.
 #'
@@ -94,34 +110,34 @@ keras$ops$lu_factor(x)
 #' mathematical 'norm', but it may still be useful for various numerical
 #' purposes. The following norms can be calculated:
 #' - For matrices:
-#'     - `ord=None`: Frobenius norm
+#'     - `ord=NULL`: Frobenius norm
 #'     - `ord="fro"`: Frobenius norm
-#'     - `ord=nuc`: nuclear norm
-#'     - `ord=np.inf`: `max(sum(abs(x), axis=1))`
-#'     - `ord=-np.inf`: `min(sum(abs(x), axis=1))`
+#'     - `ord="nuc"`: nuclear norm
+#'     - `ord=Inf`: `max(sum(abs(x), axis=2))`
+#'     - `ord=-Inf`: `min(sum(abs(x), axis=2))`
 #'     - `ord=0`: not supported
-#'     - `ord=1`: `max(sum(abs(x), axis=0))`
-#'     - `ord=-1`: `min(sum(abs(x), axis=0))`
+#'     - `ord=1`: `max(sum(abs(x), axis=1))`
+#'     - `ord=-1`: `min(sum(abs(x), axis=1))`
 #'     - `ord=2`: 2-norm (largest sing. value)
 #'     - `ord=-2`: smallest singular value
 #'     - other: not supported
 #' - For vectors:
-#'     - `ord=None`: 2-norm
+#'     - `ord=NULL`: 2-norm
 #'     - `ord="fro"`: not supported
-#'     - `ord=nuc`: not supported
-#'     - `ord=np.inf`: `max(abs(x))`
-#'     - `ord=-np.inf`: `min(abs(x))`
+#'     - `ord="nuc"`: not supported
+#'     - `ord=Inf`: `max(abs(x))`
+#'     - `ord=-Inf`: `min(abs(x))`
 #'     - `ord=0`: `sum(x != 0)`
 #'     - `ord=1`: as below
 #'     - `ord=-1`: as below
 #'     - `ord=2`: as below
 #'     - `ord=-2`: as below
-#'     - other: `sum(abs(x)**ord)**(1./ord)`
+#'     - other: `sum(abs(x)^ord)^(1/ord)`
 #'
 #' # Examples
-#' ```python
-#' x = keras.ops.reshape(keras.ops.arange(9, dtype="float32") - 4, (3, 3))
-#' keras.ops.linalg.norm(x)
+#' ```{r}
+#' x <- op_reshape(op_arange(9, dtype="float32") - 4, c(3, 3))
+#' op_norm(x)
 #' # 7.7459664
 #' ```
 #'
@@ -132,30 +148,41 @@ keras$ops$lu_factor(x)
 #' Input tensor.
 #'
 #' @param ord
-#' Order of the norm (see table under Notes). The default is `None`.
+#' Order of the norm (see table under Notes). The default is `NULL`.
 #'
 #' @param axis
 #' If `axis` is an integer, it specifies the axis of `x` along which
-#' to compute the vector norms. If `axis` is a 2-tuple, it specifies
+#' to compute the vector norms. If `axis` is a length 2 vector, it specifies
 #' the axes that hold 2-D matrices, and the matrix norms of these
 #' matrices are computed.
 #'
 #' @param keepdims
-#' If this is set to `True`, the axes which are reduced are left
+#' If this is set to `TRUE`, the axes which are reduced are left
 #' in the result as dimensions with size one.
 #'
 #' @export
+#' @family linear algebra ops
+#' @family ops
 #' @tether keras.ops.norm
-#' @seealso
-#' + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/norm>
+# @seealso
+# + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/norm>
 op_norm <-
 function (x, ord = NULL, axis = NULL, keepdims = FALSE)
 {
-    args <- capture_args(list(axis = as_axis))
+    args <- capture_args(list(
+      axis = as_axis,
+      ord = function(x) {
+        if (is.double(x) && all(!is.infinite(x)))
+          as.integer(x)
+        else
+          x
+      }
+    ))
     do.call(keras$ops$norm, args)
 }
 
-#' Solves a linear system of equations given by `a x = b`.
+
+#' Solves a linear system of equations given by `a %*% x = b`.
 #'
 #' @returns
 #' A tensor of shape `(..., M)` or `(..., M, N)` representing the solution
@@ -169,25 +196,33 @@ function (x, ord = NULL, axis = NULL, keepdims = FALSE)
 #' right-hand side or "dependent variable" matrix.
 #'
 #' @export
+#' @family linear algebra ops
+#' @family ops
 #' @tether keras.ops.solve_triangular
-#' @seealso
-#' + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/solve_triangular>
+# @seealso
+# + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/solve_triangular>
 op_solve_triangular <-
 function (a, b, lower = FALSE)
 keras$ops$solve_triangular(a, b, lower)
 
+
 #' Computes the singular value decomposition of a matrix.
 #'
 #' @returns
-#' A tuple of three tensors: a tensor of shape `(..., M, M)` containing the
-#' left singular vectors, a tensor of shape `(..., M, N)` containing the
-#' singular values and a tensor of shape `(..., N, N)` containing the
-#' right singular vectors.
+#' A list of three tensors:
+#' - a tensor of shape `(..., M, M)` containing the
+#'   left singular vectors,
+#' - a tensor of shape `(..., M, N)` containing the
+#'   singular values and
+#' - a tensor of shape `(..., N, N)` containing the
+#'   right singular vectors.
 #'
 #' @param x
 #' Input tensor of shape `(..., M, N)`.
 #'
 #' @export
+#' @family linear algebra ops
+#' @family ops
 #' @tether keras.ops.svd
 #' @seealso
 #' + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/svd>
