03c3a81e21e40f6614f147db4cd92433425ee165
Tomasz Kalinowski <kalinowskit@gmail.com>
2024-02-19 13:43:09 -0500

    adapt new numpy ops

--- R/ops-numpy.R
+++ R/ops-numpy.R
@@ -1,7 +1,7 @@
 #' Safe element-wise division which returns 0 where the denominator is 0.
 #'
 #' @returns
-#'     The quotient `x1/x2`, element-wise, with zero where x2 is zero.
+#' The quotient `x1/x2`, element-wise, with zero where x2 is zero.
 #'
 #' @param x1
 #' First input tensor.
@@ -10,9 +10,11 @@
 #' Second input tensor.
 #'
 #' @export
+#' @family numpy ops
+#' @family ops
 #' @tether keras.ops.divide_no_nan
-#' @seealso
-#' + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/divide_no_nan>
+# @seealso
+# + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/divide_no_nan>
 op_divide_no_nan <-
 function (x1, x2)
 keras$ops$divide_no_nan(x1, x2)
