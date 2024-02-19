10feb96ac3861873620bcfa777bcc915512a70f3
Tomasz Kalinowski <kalinowskit@gmail.com>
2024-02-19 13:41:51 -0500

    adapt new math op_erfinv

--- R/ops-math.R
+++ R/ops-math.R
@@ -3,10 +3,9 @@
 #' @description
 #'
 #' # Examples
-#' ```python
-#' x = np.array([-0.5, -0.2, -0.1, 0.0, 0.3])
-#' keras.ops.erfinv(x)
-#' # array([-0.47694, -0.17914, -0.08886,  0. ,  0.27246], dtype=float32)
+#' ```{r}
+#' x <- op_array(c(-0.5, -0.2, -0.1, 0.0, 0.3))
+#' op_erfinv(x)
 #' ```
 #'
 #' @returns
@@ -16,9 +15,11 @@
 #' Input tensor.
 #'
 #' @export
+#' @family math ops
+#' @family ops
 #' @tether keras.ops.erfinv
-#' @seealso
-#' + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/erfinv>
+# @seealso
+# + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/erfinv>
 op_erfinv <-
 function (x)
 keras$ops$erfinv(x)
