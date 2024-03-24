ffb95d2915b8592a5221fedf4e76dbfafec2aabc
Tomasz Kalinowski <kalinowskit@gmail.com>
2024-03-23 21:54:35 -0400

    add `layer_mel_spectrogram()`

--- R/layers-preprocessing.R
+++ R/layers-preprocessing.R
@@ -2353,6 +2353,134 @@ function(object, run_eagerly = NULL, steps_per_execution = NULL, ...) {
 }
 
 
+#' A preprocessing layer to convert raw audio signals to Mel spectrograms.
+#'
+#' @description
+#' This layer takes `float32`/`float64` single or batched audio signal as
+#' inputs and computes the Mel spectrogram using Short-Time Fourier Transform
+#' and Mel scaling. The input should be a 1D (unbatched) or 2D (batched) tensor
+#' representing audio signals. The output will be a 2D or 3D tensor
+#' representing Mel spectrograms.
+#'
+#' A spectrogram is an image-like representation that shows the frequency
+#' spectrum of a signal over time. It uses x-axis to represent time, y-axis to
+#' represent frequency, and each pixel to represent intensity.
+#' Mel spectrograms are a special type of spectrogram that use the mel scale,
+#' which approximates how humans perceive sound. They are commonly used in
+#' speech and music processing tasks like speech recognition, speaker
+#' identification, and music genre classification.
+#'
+#' # References
+#' - [Spectrogram](https://en.wikipedia.org/wiki/Spectrogram),
+#' - [Mel scale](https://en.wikipedia.org/wiki/Mel_scale).
+#'
+#' # Examples
+#' **Unbatched audio signal**
+#'
+#' ```r
+#' layer <- layer_mel_spectrogram(
+#'   num_mel_bins = 64,
+#'   sampling_rate = 8000,
+#'   sequence_stride = 256,
+#'   fft_length = 2048
+#' )
+#' layer(random_uniform(shape = c(16000))) |> shape()
+#' ```
+#'
+#' **Batched audio signal**
+#'
+#' ```r
+#' layer <- layer_mel_spectrogram(
+#'   num_mel_bins = 80,
+#'   sampling_rate = 8000,
+#'   sequence_stride = 128,
+#'   fft_length = 2048
+#' )
+#' layer(random_uniform(shape = c(2, 16000))) |> shape()
+#' ```
+#'
+#' # Input Shape
+#' 1D (unbatched) or 2D (batched) tensor with shape:`(..., samples)`.
+#'
+#' # Output Shape
+#' 2D (unbatched) or 3D (batched) tensor with
+#' shape:`(..., num_mel_bins, time)`.
+#'
+#' @param fft_length
+#' Integer, size of the FFT window.
+#'
+#' @param sequence_stride
+#' Integer, number of samples between successive STFT
+#' columns.
+#'
+#' @param sequence_length
+#' Integer, size of the window used for applying
+#' `window` to each audio frame. If `NULL`, defaults to `fft_length`.
+#'
+#' @param window
+#' String, name of the window function to use. Available values
+#' are `"hann"` and `"hamming"`. If `window` is a tensor, it will be
+#' used directly as the window and its length must be
+#' `sequence_length`. If `window` is `NULL`, no windowing is
+#' used. Defaults to `"hann"`.
+#'
+#' @param sampling_rate
+#' Integer, sample rate of the input signal.
+#'
+#' @param num_mel_bins
+#' Integer, number of mel bins to generate.
+#'
+#' @param min_freq
+#' Float, minimum frequency of the mel bins.
+#'
+#' @param max_freq
+#' Float, maximum frequency of the mel bins.
+#' If `NULL`, defaults to `sampling_rate / 2`.
+#'
+#' @param power_to_db
+#' If TRUE, convert the power spectrogram to decibels.
+#'
+#' @param top_db
+#' Float, minimum negative cut-off `max(10 * log10(S)) - top_db`.
+#'
+#' @param mag_exp
+#' Float, exponent for the magnitude spectrogram.
+#' 1 for magnitude, 2 for power, etc. Default is 2.
+#'
+#' @param ref_power
+#' Float, the power is scaled relative to it
+#' `10 * log10(S / ref_power)`.
+#'
+#' @param min_power
+#' Float, minimum value for power and `ref_power`.
+#'
+#' @param object
+#' Object to compose the layer with. A tensor, array, or sequential model.
+#'
+#' @param ...
+#' For forward/backward compatability.
+#'
+#' @inherit layer_dense return
+#' @family audio preprocessing layers
+#' @family preprocessing layers
+#' @family layers
+#' @export
+#' @tether keras.layers.MelSpectrogram
+layer_mel_spectrogram <-
+function (object, fft_length = 2048L, sequence_stride = 512L,
+    sequence_length = NULL, window = "hann", sampling_rate = 16000L,
+    num_mel_bins = 128L, min_freq = 20, max_freq = NULL, power_to_db = TRUE,
+    top_db = 80, mag_exp = 2, min_power = 1e-10, ref_power = 1,
+    ...)
+{
+    args <- capture_args(list(fft_length = as_integer, sequence_stride = as_integer,
+        sequence_length = as_integer, sampling_rate = as_integer,
+        num_mel_bins = as_integer, input_shape = normalize_shape,
+        batch_size = as_integer, batch_input_shape = normalize_shape),
+        ignore = "object")
+    create_layer(keras$layers$MelSpectrogram, object, args)
+}
+
 
 
 
