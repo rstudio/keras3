68f4fc39278e6dac0d8c200fd9fe6efac6d0f74c
Tomasz Kalinowski <kalinowskit@gmail.com>
2024-06-25 11:17:01 -0400

    op_image_map_coordinates: first arg rename, doc improvements

--- R/ops-image.R
+++ R/ops-image.R
@@ -175,7 +175,7 @@ function (images, size, strides = NULL, dilation_rate = 1L, padding = "valid",
 }
 
 
-#' Map the input array to new coordinates by interpolation..
+#' Map the input array to new coordinates by interpolation.
 #'
 #' @description
 #' Note that interpolation near boundaries differs from the scipy function,
@@ -183,13 +183,13 @@ function (images, size, strides = NULL, dilation_rate = 1L, padding = "valid",
 #' [scipy/issues/2640](https://github.com/scipy/scipy/issues/2640).
 #'
 #' @returns
-#' Output image or batch of images.
+#' Output input or batch of inputs.
 #'
-#' @param input
+#' @param inputs
 #' The input array.
 #'
 #' @param coordinates
-#' The coordinates at which input is evaluated.
+#' The coordinates at which `inputs` is evaluated.
 #'
 #' @param order
 #' The order of the spline interpolation. The order must be `0` or
@@ -197,27 +197,27 @@ function (images, size, strides = NULL, dilation_rate = 1L, padding = "valid",
 #' interpolation.
 #'
 #' @param fill_mode
-#' Points outside the boundaries of the input are filled
+#' Points outside the boundaries of the inputs are filled
 #' according to the given mode. Available methods are `"constant"`,
 #' `"nearest"`, `"wrap"` and `"mirror"` and `"reflect"`. Defaults to
 #' `"constant"`.
 #' - `"constant"`: `(k k k k | a b c d | k k k k)`
-#'     The input is extended by filling all values beyond
+#'     inputs is extended by filling all values beyond
 #'     the edge with the same constant value k specified by
 #'     `fill_value`.
 #' - `"nearest"`: `(a a a a | a b c d | d d d d)`
-#'     The input is extended by the nearest pixel.
+#'     inputs is extended by the nearest pixel.
 #' - `"wrap"`: `(a b c d | a b c d | a b c d)`
-#'     The input is extended by wrapping around to the opposite edge.
+#'     inputs is extended by wrapping around to the opposite edge.
 #' - `"mirror"`: `(c d c b | a b c d | c b a b)`
-#'     The input is extended by mirroring about the edge.
+#'     inputs is extended by mirroring about the edge.
 #' - `"reflect"`: `(d c b a | a b c d | d c b a)`
-#'     The input is extended by reflecting about the edge of the last
+#'     inputs is extended by reflecting about the edge of the last
 #'     pixel.
 #'
 #' @param fill_value
-#' Value used for points outside the boundaries of the input if
-#' `fill_mode = "constant"`. Defaults to `0`.
+#' Value used for points outside the boundaries of the inputs
+#' if `fill_mode = "constant"`. Defaults to `0`.
 #'
 #' @export
 #' @family image ops
@@ -227,10 +227,12 @@ function (images, size, strides = NULL, dilation_rate = 1L, padding = "valid",
 #  + <https://www.tensorflow.org/api_docs/python/tf/keras/ops/image/map_coordinates>
 #' @tether keras.ops.image.map_coordinates
 op_image_map_coordinates <-
-function (input, coordinates, order, fill_mode = "constant",
+function (inputs, coordinates, order, fill_mode = "constant",
     fill_value = 0L)
 {
     args <- capture_args(list(fill_value = as_integer))
+    # "inputs" renamed from "input" in Keras 3.4.0
+    args <- args_to_positional(args, "inputs")
     do.call(keras$ops$image$map_coordinates, args)
 }
 
