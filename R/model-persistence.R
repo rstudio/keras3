db8579e5d836a4881de80fe53d4a815cbc41cd66
Tomasz Kalinowski <kalinowskit@gmail.com>
2024-10-16 13:00:57 -0400

    attempt to wrap `KerasFileEditor`

--- R/model-persistence.R
+++ R/model-persistence.R
@@ -892,6 +892,165 @@ with_custom_object_scope <- function(objects, expr) {
 }
 
 
+## attempting to use keras.saving.KerasFileEditor with the most basic example raised an error -
+## it seem not ready for primetime. Revisit in next release.
+## Also, when exporting, add a `print()` method that calls summary.
+#' @noRd
+#'
+#'
+#' Utility to inspect, edit, and resave Keras weights files.
+#'
+#' @description
+#' You will find this class useful when adapting
+#' an old saved weights file after having made
+#' architecture changes to a model.
+#'
+#' # Examples
+#' ```{r}
+#' model <- keras_model_sequential(name = "my_sequential",
+#'                                 input_shape = c(2, 2),
+#'                                 input_name = "my_input") |>
+#'   layer_dense(2, activation = "sigmoid", name = "my_dense") |>
+#'   layer_dense(2, activation = "sigmoid", name = "my_dense2")
+#'
+#' model |> compile(optimizer="adam", loss="mse", metrics=c("mae"))
+#' model |> fit(matrix(1:2, nrow = 1), matrix(1:2, nrow = 1), verbose = 0)
+#'
+#' path.keras <- tempfile("model-", fileext = ".keras")
+#' path.weights.h5 <-  tempfile("model-", fileext = ".weights.h5")
+#' model |> save_model(path.keras)
+#' model |> save_model_weights(path.weights.h5)
+#' #' #'
+#' #' #'
+#' #' #'
+#' editor = saved_keras_file_editor(path.keras)
+#' editor = saved_keras_file_editor(path.weights.h5)
+#'
+#' # Displays current contents
+#' editor$summary()
+#'
+#' # Remove the weights of an existing layer
+#' editor$delete_object("layers/dense_2")
+#'
+#' # Add the weights of a new layer
+#' editor$add_object("layers/einsum_dense", weights=list("0"= ..., "1"= ...))
+#'
+#' # Save the weights of the edited model
+#' editor$resave_weights("edited_model.weights.h5")
+#' ```
+#'
+#' Methods defined:
+#'
+#' * ```r
+#'   add_object(object_path, weights)
+#'   ```
+#'   Add a new object to the file (e.g. a layer).
+#'
+#'   Args:
+#'   * `object_path`: String, full path of the
+#'       object to add (e.g. `"layers/dense_2"`).
+#'   * `weights`: Named list or dictionary mapping weight names to weight
+#'       values (arrays),
+#'       e.g. `list("0" = kernel_value, "1" = bias_value)`.
+#'
+#' * ```r
+#'   add_weights(object_name, weights)
+#'   ```
+#'   Add one or more new weights to an existing object.
+#'
+#'   Args:
+#'   * `object_name`: String, name or path of the
+#'      object to add the weights to
+#'      (e.g. `"dense_2"` or `"layers/dense_2"`).
+#'   * `weights`: Named list or dict mapping weight names to weight
+#'      values (arrays),
+#'      e.g. `list("0" = kernel_value, "1" = bias_value)`.
+#'
+#' * ```r
+#'   compare(reference_model)
+#'   ```
+#'   Compares the opened file to a reference model.
+#'
+#'   This method will list all mismatches between the
+#'   currently opened file and the provided reference model.
+#'
+#'   Args:
+#'   * `reference_model`: Model instance to compare to.
+#'
+#'   Returns:
+#'
+#'   Named list with the following names:
+#'   `'status'`, `'error_count'`, `'match_count'`.
+#'   Status can be `'success'` or `'error'`.
+#'   `'error_count'` is the number of mismatches found.
+#'   `'match_count'` is the number of matching weights found.
+#'
+#' * ```r
+#'   delete_object(object_name)
+#'   ```
+#'   Removes an object from the file (e.g. a layer).
+#'
+#'   Args:
+#'   * `object_name`: String, name or path of the
+#'       object to delete (e.g. `"dense_2"` or
+#'       `"layers/dense_2"`).
+#'
+#' * ```r
+#'   delete_weight(object_name, weight_name)
+#'   ```
+#'   Removes a weight from an existing object.
+#'
+#'   Args:
+#'   * `object_name`: String, name or path of the
+#'      object from which to remove the weight
+#'      (e.g. `"dense_2"` or `"layers/dense_2"`).
+#'   * `weight_name`: String, name of the weight to
+#'       delete (e.g. `"0"`).
+#'
+#' * ```r
+#'   rename_object(object_name, new_name)
+#'   ```
+#'   Rename an object in the file (e.g. a layer).
+#'
+#'   Args:
+#'   * `object_name`: String, name or path of the
+#'       object to rename (e.g. `"dense_2"` or
+#'       `"layers/dense_2"`).
+#'   * `new_name`: String, new name of the object.
+#'
+#' * ```r
+#'   resave_weights(filepath)
+#'   ```
+#'
+#' * ```r
+#'   save(filepath)
+#'   ```
+#'   Save the edited weights file.
+#'
+#'   Args:
+#'   * `filepath`: Path to save the file to.
+#'       Must be a `.weights.h5` file.
+#'
+#' * ```r
+#'   summary()
+#'   ```
+#'   Prints the weight structure of the opened file.
+#'
+#'
+#'
+#' @param filepath
+#' The path to a local file to inspect and edit.
+#'
+#' @export
+#' @tether keras.saving.KerasFileEditor
+#'
+saved_keras_file_editor <-
+function (filepath)
+{
+    keras$saving$KerasFileEditor(filepath)
+}
+
+
 # ---- internal utilities ----
 
 normalize_custom_objects <- function(objects) {
