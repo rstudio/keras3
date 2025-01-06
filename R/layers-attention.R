db21331d326e35fd3d0a876dcabd0e3b112854cb
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-01-06 13:46:20 -0500

    new arg `layer_multi_head_attention`:  `flash_attention`

--- R/layers-attention.R
+++ R/layers-attention.R
@@ -388,6 +388,13 @@ function (object, head_dim, num_query_heads, num_key_value_heads,
 #' axes over which the attention is applied. `NULL` means
 #' attention over all axes, but batch, heads, and features.
 #'
+#' @param flash_attention
+#' If `NULL`, the layer attempts to use flash
+#' attention for faster and more memory-efficient attention
+#' computations when possible. This behavior can be configured using
+#' `config_enable_flash_attention()` or
+#' `config_disable_flash_attention()`.
+#'
 #' @param kernel_initializer
 #' Initializer for dense layer kernels.
 #'
@@ -428,7 +435,7 @@ function (object, head_dim, num_query_heads, num_key_value_heads,
 #' @tether keras.layers.MultiHeadAttention
 layer_multi_head_attention <-
 function (inputs, num_heads, key_dim, value_dim = NULL, dropout = 0,
-    use_bias = TRUE, output_shape = NULL, attention_axes = NULL,
+    use_bias = TRUE, output_shape = NULL, attention_axes = NULL, flash_attention=NULL,
     kernel_initializer = "glorot_uniform", bias_initializer = "zeros",
     kernel_regularizer = NULL, bias_regularizer = NULL, activity_regularizer = NULL,
     kernel_constraint = NULL, bias_constraint = NULL, seed = NULL, ...)
