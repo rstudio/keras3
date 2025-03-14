442a7758f1558acb2520b9e2c85baf9bd7e76cd9
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-03-14 13:52:34 -0400

    export `newaxis`

--- R/package.R
+++ R/package.R
@@ -378,3 +378,25 @@ is_keras_available <- function(version = NULL) {
 # TODO: add option in `is_keras_available()` to avoid initializing Python
 #       (maybe in a callr call?), reexport.
 # TODO: add func `is_backend_available()`, usage `is_backend_available("tensorflow")`
+
+
+#' New axis
+#'
+#' This is an alias for `NULL`. It is meant to be used in `[` on tensors,
+#' to expand dimensions of a tensor
+#'
+#' ```r
+#' x <- op_convert_to_tensor(1:10)
+#'
+#' op_shape(x)
+#' op_shape(x[])
+#' op_shape(x[newaxis])
+#' op_shape(x@py[newaxis])
+#' op_shape(x@r[newaxis])
+#'
+#' op_shape(x[newaxis, .., newaxis])
+#' op_shape(x@py[newaxis, .., newaxis])
+#' op_shape(x@r[newaxis, .., newaxis])
+#' ````
+#' @export
+newaxis <- NULL
