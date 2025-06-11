b460e1546edaf472c92a595450fba4886861f526
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-06-11 17:49:36 -0400

    add `as.array` and `as.double` jax methods

--- R/jax-methods.R
+++ R/jax-methods.R
@@ -1,10 +1,22 @@
 
 #' @export
-as.array.jaxlib.xla_extension.ArrayImpl <- function(x, ...) {
+as.array.jax.Array <- function(x, ...) {
   import("numpy")$asarray(x)
 }
 
 #' @export
-as.double.jaxlib.xla_extension.ArrayImpl <- function(x, ...) {
+as.array.jaxlib._jax.ArrayImpl <- as.array.jax.Array
+
+#' @export
+as.array.jaxlib.xla_extension.ArrayImpl <- as.array.jax.Array
+
+#' @export
+as.double.jax.Array <- function(x, ...) {
   as.double(import("numpy")$asarray(x))
 }
+
+#' @export
+as.double.jaxlib._jax.ArrayImpl <- as.double.jax.Array
+
+#' @export
+as.double.jaxlib.xla_extension.ArrayImpl <- as.double.jax.Array
