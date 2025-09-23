3b9fef18f83b70cb8d3c562c5eb137af94645669
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-09-23 17:16:39 -0400

    callback_lambda: docs

--- R/callbacks.R
+++ R/callbacks.R
@@ -251,8 +251,8 @@ function (monitor = "val_loss", min_delta = 0L, patience = 0L,
 #'   `epoch`, `logs`
 #' - `on_train_begin` and `on_train_end` expect one positional argument:
 #'   `logs`
-#' - `on_train_batch_begin` and `on_train_batch_end` expect two positional
-#'   arguments: `batch`, `logs`
+#' - `on_train_batch_begin` and `on_train_batch_end` expect a positional
+#'   argument `batch` and a keyword argument `logs`
 #' - See `Callback` class definition for the full list of functions and their
 #'   expected arguments.
 #'
@@ -534,9 +534,8 @@ function (schedule, verbose = 0L)
 #' decision to overwrite the current save file is made based on either
 #' the maximization or the minimization of the monitored quantity.
 #' For `val_acc`, this should be `"max"`, for `val_loss` this should be
-#' `"min"`, etc. In `"auto"` mode, the mode is set to `"max"` if the
-#' quantities monitored are `"acc"` or start with `"fmeasure"` and are
-#' set to `"min"` for the rest of the quantities.
+#' `"min"`, etc. In `"auto"` mode, the direction is automatically
+#' inferred from the name of the monitored quantity.
 #'
 #' @param save_weights_only
 #' if TRUE, then only the model's weights will be saved
@@ -995,4 +994,3 @@ normalize_callbacks_with_metrics <- function(view_metrics, initial_epoch, callba
 
   callbacks
 }
-
