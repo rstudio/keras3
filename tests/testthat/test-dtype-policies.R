1533b39631e7eabc23e392e755e1f264a3209cf0
Tomasz Kalinowski <kalinowskit@gmail.com>
2026-07-31 10:08:19 -0400

    add quantized dtype policies

---
+++ tests/testthat/test-dtype-policies.R
@@ -0,0 +1,11 @@
+test_that("quantized dtype policy constructors expose public APIs", {
+  policies <- list(
+    dtype_policy_awq("awq/4/128"),
+    dtype_policy_gptq("gptq/4/128"),
+    dtype_policy_int4("int4/128")
+  )
+
+  expect_s3_class(policies[[1]], "keras.src.dtype_policies.dtype_policy.AWQDTypePolicy")
+  expect_s3_class(policies[[2]], "keras.src.dtype_policies.dtype_policy.GPTQDTypePolicy")
+  expect_s3_class(policies[[3]], "keras.src.dtype_policies.dtype_policy.Int4DTypePolicy")
+})
